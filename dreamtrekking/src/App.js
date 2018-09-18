962a1b70ce53fe315225786e6fc2c09f70755172
Jacob Leon Lyerla <umasour@yahoo.com>
2018-09-17 23:17:30 -0700

    set up less, ran a few test

--- dreamtrekking/src/App.js
+++ dreamtrekking/src/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
-
+import './css/index.css'
 class App extends Component {
   render() {
     return (
-      <div className="App">
-       test
+      <div className="App mapBackground-styles">
+   
+      <div className="box">
+      i love this box
+      </div>
+      <div className="two">another div</div>
       </div>
     );
   }
--- dreamtrekking/src/index.js
+++ dreamtrekking/src/index.js
@@ -1,10 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
-import './index.css';
-import App from './App';
+import "./index.css";
+import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
-ReactDOM.render(<Router>
+ReactDOM.render(
+  <Router>
     <App />
   </Router>,
   document.getElementById("root")
