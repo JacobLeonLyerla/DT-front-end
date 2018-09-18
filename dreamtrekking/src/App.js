423bfc5e002e30d1aeb249d66f20fb8433f7c04a
Jacob Leon Lyerla <umasour@yahoo.com>
2018-09-17 23:23:55 -0700

    added in and positioned a reactstrap card component

--- dreamtrekking/src/App.js
+++ dreamtrekking/src/App.js
@@ -1,15 +1,21 @@
 import React, { Component } from 'react';
+import { Card, CardImg, CardText, CardBody,
+  CardTitle, CardSubtitle, Button } from 'reactstrap';
 import './App.css';
 import './css/index.css'
 class App extends Component {
   render() {
     return (
       <div className="App mapBackground-styles">
-   
-      <div className="box">
-      i love this box
-      </div>
-      <div className="two">another div</div>
+      <br/>
+    <Card>
+        <CardBody>
+          <CardTitle>Card title</CardTitle>
+          <CardSubtitle>Card subtitle</CardSubtitle>
+          <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
+          <Button>Button</Button>
+        </CardBody>
+      </Card>
       </div>
     );
   }
