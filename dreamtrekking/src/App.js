c112be27df31af49c3d6531e23db9f390bb3ee00
Jacob Leon Lyerla <umasour@yahoo.com>
2018-09-18 23:38:51 -0700

    stripped out react-share, i'll add it back in once the rest of the site is built out.

--- dreamtrekking/src/App.js
+++ dreamtrekking/src/App.js
@@ -8,40 +8,7 @@ import {
   CardSubtitle,
   Button
 } from "reactstrap";
-import {
-  FacebookShareButton,
-  GooglePlusShareButton,
-  LinkedinShareButton,
-  TwitterShareButton,
-  TelegramShareButton,
-  WhatsappShareButton,
-  PinterestShareButton,
-  VKShareButton,
-  OKShareButton,
-  RedditShareButton,
-  TumblrShareButton,
-  LivejournalShareButton,
-  MailruShareButton,
-  ViberShareButton,
-  WorkplaceShareButton,
-  EmailShareButton,
-  FacebookIcon,
-  TwitterIcon,
-  TelegramIcon,
-  WhatsappIcon,
-  GooglePlusIcon,
-  LinkedinIcon,
-  PinterestIcon,
-  VKIcon,
-  OKIcon,
-  RedditIcon,
-  TumblrIcon,
-  LivejournalIcon,
-  MailruIcon,
-  ViberIcon,
-  WorkplaceIcon,
-  EmailIcon
-} from "react-share";
+
 import "./App.css";
 import "./css/index.css";
 import { react } from "babel-types";
@@ -59,27 +26,7 @@ class App extends Component {
               bulk of the card's content.
             </CardText>
             <Button>Button</Button>
-            <div className="share-styles">
-              <EmailShareButton>
-                <EmailIcon  round={true} />
-              </EmailShareButton>
-              <RedditShareButton windowWidth={"900"}   children={react.node} url={"https://www.reddit.com/submit?url=http%3A%2F%2Fgithub.com&title=GitHub"}>
-                <RedditIcon  round={true} />
-              </RedditShareButton>
-              <LinkedinShareButton
-                children={react.node}
-                url={
-                  "https://www.linkedin.com/shareArticle?mini=true&url=http://developer.linkedin.com&title=LinkedIn%20Developer%20Network&summary=My%20favorite%20developer%20program&source=LinkedIn"
-                }
-                title={"https://dungeon-run.netlify.com/"}
-                description={"sweet ass game"}
-              >
-                <LinkedinIcon  round={true} />
-              </LinkedinShareButton>
-              <TwitterShareButton children={react.node} url={"https://dungeon-run.netlify.com"}>
-                <TwitterIcon  round={true} />
-              </TwitterShareButton>
-            </div>
+
           </CardBody>
         </Card>
       </div>
