e3ca184a915437caed53740dc235ef4e8fdd81de
Jacob Leon Lyerla <umasour@yahoo.com>
2018-09-21 00:23:13 -0700

    going to come back to this

--- dreamtrekking/src/components/landing.js
+++ dreamtrekking/src/components/landing.js
@@ -1,7 +1,7 @@
 import React, { Fragment} from "react";
 import Header from "./header"
-import { Segment, Image } from 'semantic-ui-react'
-import logo from "../assets/logo.jpg"
+import { Segment, Image,Container } from 'semantic-ui-react'
+import logo from "../assets/castle.jpg"
 const Landing =()=> {
 
   //   componentDidMount() {
@@ -15,15 +15,16 @@ const Landing =()=> {
         <br/>
         <Header/>
       </div>
-      <div className="sell"><div className="shroud">  <Segment>
-    <Image src={logo} size='small' floated='left' />
+      <div className="sell"><Container size='massive' className="shroud">  <Segment  size='massive'>
+    <Image src={logo} size='large' floated='left' />
     <p>
       Te eum doming eirmod, nominati pertinacia argumentum ad his. Ex eam alia facete scriptorem,
       est autem aliquip detraxit at. Usu ocurreret referrentur at, cu epicurei appellantur vix. Cum
       ea laoreet recteque electram, eos choro alterum definiebas in. Vim dolorum definiebas an. Mei
       ex natum rebum iisque.
     </p>
-    <Image src={logo} size='small' floated='right' />
+
+    <Image src={logo} size='big' floated='right' />
     <p>
       Audiam quaerendum eu sea, pro omittam definiebas ex. Te est latine definitiones. Quot wisi
       nulla ex duo. Vis sint solet expetenda ne, his te phaedrum referrentur consectetuer. Id vix
@@ -35,7 +36,20 @@ const Landing =()=> {
       utroque quo. Populo quodsi te eam, wisi everti eos ex, eum elitr altera utamur at. Quodsi
       convenire mnesarchum eu per, quas minimum postulant per id.
     </p>
-  </Segment></div></div>
+
+    <Image src={logo} size='big' floated='left' />
+    <p>
+      Audiam quaerendum eu sea, pro omittam definiebas ex. Te est latine definitiones. Quot wisi
+      nulla ex duo. Vis sint solet expetenda ne, his te phaedrum referrentur consectetuer. Id vix
+      fabulas oporteat, ei quo vide phaedrum, vim vivendum maiestatis in.
+    </p>
+    <p>
+      Eu quo homero blandit intellegebat. Incorrupte consequuntur mei id. Mei ut facer dolores
+      adolescens, no illum aperiri quo, usu odio brute at. Qui te porro electram, ea dico facete
+      utroque quo. Populo quodsi te eam, wisi everti eos ex, eum elitr altera utamur at. Quodsi
+      convenire mnesarchum eu per, quas minimum postulant per id.
+    </p>
+  </Segment></Container></div>
    </Fragment> );
   
 }
