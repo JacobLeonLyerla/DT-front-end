ee13846d05e43c7d39d6f302223b96321c3d4ff0
Jacob Leon Lyerla <umasour@yahoo.com>
2018-09-20 23:57:14 -0700

    added in some text

--- dreamtrekking/src/components/landing.js
+++ dreamtrekking/src/components/landing.js
@@ -1,5 +1,7 @@
 import React, { Fragment} from "react";
 import Header from "./header"
+import { Segment, Image } from 'semantic-ui-react'
+import logo from "../assets/logo.jpg"
 const Landing =()=> {
 
   //   componentDidMount() {
@@ -13,7 +15,27 @@ const Landing =()=> {
         <br/>
         <Header/>
       </div>
-      <div className="sell"><div className="shroud"></div></div>
+      <div className="sell"><div className="shroud">  <Segment>
+    <Image src={logo} size='small' floated='left' />
+    <p>
+      Te eum doming eirmod, nominati pertinacia argumentum ad his. Ex eam alia facete scriptorem,
+      est autem aliquip detraxit at. Usu ocurreret referrentur at, cu epicurei appellantur vix. Cum
+      ea laoreet recteque electram, eos choro alterum definiebas in. Vim dolorum definiebas an. Mei
+      ex natum rebum iisque.
+    </p>
+    <Image src={logo} size='small' floated='right' />
+    <p>
+      Audiam quaerendum eu sea, pro omittam definiebas ex. Te est latine definitiones. Quot wisi
+      nulla ex duo. Vis sint solet expetenda ne, his te phaedrum referrentur consectetuer. Id vix
+      fabulas oporteat, ei quo vide phaedrum, vim vivendum maiestatis in.
+    </p>
+    <p>
+      Eu quo homero blandit intellegebat. Incorrupte consequuntur mei id. Mei ut facer dolores
+      adolescens, no illum aperiri quo, usu odio brute at. Qui te porro electram, ea dico facete
+      utroque quo. Populo quodsi te eam, wisi everti eos ex, eum elitr altera utamur at. Quodsi
+      convenire mnesarchum eu per, quas minimum postulant per id.
+    </p>
+  </Segment></div></div>
    </Fragment> );
   
 }
