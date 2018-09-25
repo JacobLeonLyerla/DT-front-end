803e850929ac4d656d8cd43f086fdf901af32e0b
Jacob Leon Lyerla <umasour@yahoo.com>
2018-09-24 23:40:22 -0700

    basic dashboard nav in bootstrap done

--- dreamtrekking/src/components/nav.js
+++ dreamtrekking/src/components/nav.js
@@ -1,18 +1,50 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
 
-import logo from "../assets/logoinit.png"
+Media } from 'reactstrap';
+  import logo from "../assets/logoinit.png";
 
-  class  DTNav extends Component {
-  state = {}
+class DTNav extends Component {
+   constructor(props) {
+    super(props);
 
- 
-
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      isOpen: false
+    };
+  }
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
   render() {
-
-
     return (
-  <div>asda</div>
-    )
+      <div>
+      <Navbar color="none" light expand="md">
+        <NavbarBrand href="/"><Media className="navLogo-styles" src={logo}/></NavbarBrand>
+        <NavbarToggler onClick={this.toggle} />
+        <Collapse isOpen={this.state.isOpen} navbar>
+          <Nav className="ml-auto" navbar>
+            <NavItem>
+              <NavLink href="/components/">Components</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="https://github.com/reactstrap/reactstrap">GitHub</NavLink>
+            </NavItem>
+      
+          </Nav>
+        </Collapse>
+      </Navbar>
+    </div>
+    );
   }
-  }
-  export default DTNav
+}
+export default DTNav;
