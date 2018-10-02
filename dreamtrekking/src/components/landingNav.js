d87f6e44bc9293dac4a5e5772f65c459fcb165cd
Jacob Leon Lyerla <umasour@yahoo.com>
2018-10-01 23:37:39 -0700

    took out react-scrolling  edited nav items

--- dreamtrekking/src/components/landingNav.js
+++ dreamtrekking/src/components/landingNav.js
@@ -1,59 +1,16 @@
 import React,{Component} from 'react';
 import "../css/index.css"
-import * as Scroll from 'react-scroll';
-import { Link, Element , Events, animateScroll as scroll, scrollSpy, scroller } from 'react-scroll'
 import { Nav, NavItem, NavLink } from 'reactstrap';
  class LandingNav extends Component {
-
-  componentDidMount() {
- 
-    Events.scrollEvent.register('begin', function(to, element) {
-      console.log("begin", arguments);
-    });
- 
-    Events.scrollEvent.register('end', function(to, element) {
-      console.log("end", arguments);
-    });
- 
-    scrollSpy.update();
- 
-  }
-  componentWillUnmount() {
-    Events.scrollEvent.remove('begin');
-    Events.scrollEvent.remove('end');
-  }
-  scrollToTop() {
-    scroll.scrollToTop();
-  }
-  scrollToBottom() {
-    scroll.scrollToBottom();
-  }
-  scrollTo() {
-    scroll.scrollTo(100);
-  }
-  scrollMore() {
-    scroll.scrollMore(100);
-  }
-  handleSetActive(to) {
-    console.log(to);
-  }
-
-
   render() {
     return (
       <div className="landing-Nav">
         <Nav>
           <NavItem>
-            <NavLink href="#">Link</NavLink>
+            <NavLink className="signup" href="#">Sign Up</NavLink>
           </NavItem>
           <NavItem>
-            <NavLink href="#">Link</NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink onclick={this.scrollAbout}>About</NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink disabled href="#">Disabled Link</NavLink>
+            <NavLink href="#">Sign In</NavLink>
           </NavItem>
         </Nav>
         
