efbaff66c5c281e487f6e873f09ffb1ad9ff559e
Jacob Leon Lyerla <umasour@yahoo.com>
2018-10-01 23:50:30 -0700

    edited the lading nav

--- dreamtrekking/src/components/landingNav.js
+++ dreamtrekking/src/components/landingNav.js
@@ -1,19 +1,24 @@
-import React,{Component} from 'react';
-import "../css/index.css"
-import { Nav, NavItem, NavLink } from 'reactstrap';
- class LandingNav extends Component {
+import React, { Component } from "react";
+import "../css/index.css";
+import { Nav, NavItem, NavLink } from "reactstrap";
+import { Link } from "react-router-dom";
+class LandingNav extends Component {
   render() {
     return (
       <div className="landing-Nav">
         <Nav>
+        
+            {" "}
+            <NavItem>
+                <Link  style={{color:"white",textDecoration:"none"}} to="/signup"><NavLink className="signup" >
+                Sign Up
+              </NavLink>   </Link>
+            </NavItem>
+       
           <NavItem>
-            <NavLink className="signup" href="#">Sign Up</NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink href="#">Sign In</NavLink>
+            <NavLink>Sign In</NavLink>
           </NavItem>
         </Nav>
-        
       </div>
     );
   }
