d3b5edb7ebbd366c173e72e628f2e163449d93c1
Jacob Leon Lyerla <umasour@yahoo.com>
2018-10-02 23:26:55 -0700

    changed the way the component was formated

--- dreamtrekking/src/components/signup.js
+++ dreamtrekking/src/components/signup.js
@@ -1,43 +1,55 @@
-import React,{Component} from 'react';
-import { Form, FormGroup, Label, Input, FormFeedback, FormText } from 'reactstrap';
+import React, { Component } from "react";
+import {
+  Form,
+  FormGroup,
+  Label,
+  Input,
+  FormFeedback,
+  FormText
+} from "reactstrap";
 
 class Signup extends Component {
   render() {
     return (
-      <Form>
-       <FormGroup>
-          <Label for="exampleEmail">Input without validation</Label>
-          <Input />
-          <FormFeedback>You will not be able to see this</FormFeedback>
-          <FormText>Example help text that remains unchanged.</FormText>
-        </FormGroup>
-        <FormGroup>
-          <Label for="exampleEmail">Valid input</Label>
-          <Input valid />
-          <FormFeedback valid>Sweet! that name is available</FormFeedback>
-          <FormText>Example help text that remains unchanged.</FormText>
-        </FormGroup>
-        <FormGroup>
-          <Label for="examplePassword">Invalid input</Label>
-          <Input invalid />
-          <FormFeedback>Oh noes! that name is already taken</FormFeedback>
-          <FormText>Example help text that remains unchanged.</FormText>
-        </FormGroup>
-        <FormGroup>
-          <Label for="exampleEmail">Input without validation</Label>
-          <Input />
-          <FormFeedback tooltip>You will not be able to see this</FormFeedback>
-          <FormText>Example help text that remains unchanged.</FormText>
-        </FormGroup>
-        <FormGroup>
-          <Label for="exampleEmail">Valid input</Label>
-          <Input valid />
-          <FormFeedback valid tooltip>Sweet! that name is available</FormFeedback>
-          <FormText>Example help text that remains unchanged.</FormText>
-        </FormGroup>
-       
-      </Form>
+      <div className="signup-container">
+        <Form className="signup-form">
+          <FormGroup>
+            <Label for="exampleEmail">Input without validation</Label>
+            <Input />
+            <FormFeedback>You will not be able to see this</FormFeedback>
+            <FormText>Example help text that remains unchanged.</FormText>
+          </FormGroup>
+          <FormGroup>
+            <Label for="exampleEmail">Valid input</Label>
+            <Input valid />
+            <FormFeedback valid>Sweet! that name is available</FormFeedback>
+            <FormText>Example help text that remains unchanged.</FormText>
+          </FormGroup>
+          <FormGroup>
+            <Label for="examplePassword">Invalid input</Label>
+            <Input invalid />
+            <FormFeedback>Oh noes! that name is already taken</FormFeedback>
+            <FormText>Example help text that remains unchanged.</FormText>
+          </FormGroup>
+          <FormGroup>
+            <Label for="exampleEmail">Input without validation</Label>
+            <Input />
+            <FormFeedback tooltip>
+              You will not be able to see this
+            </FormFeedback>
+            <FormText>Example help text that remains unchanged.</FormText>
+          </FormGroup>
+          <FormGroup>
+            <Label for="exampleEmail">Valid input</Label>
+            <Input valid />
+            <FormFeedback valid tooltip>
+              Sweet! that name is available
+            </FormFeedback>
+            <FormText>Example help text that remains unchanged.</FormText>
+          </FormGroup>
+        </Form>
+      </div>
     );
   }
 }
-export default Signup
+export default Signup;
