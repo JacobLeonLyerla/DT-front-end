869a33ec64be45f4826072f7c73fdbc757a4671d
Jacob Leon Lyerla <umasour@yahoo.com>
2018-10-03 17:38:31 -0700

    setting up sign in component

---
+++ dreamtrekking/src/components/signin.js
@@ -0,0 +1,59 @@
+import React, { Component } from "react";
+import {
+  Form,
+  FormGroup,
+  Label,
+  Input,
+  FormFeedback,
+  FormText,
+  Button
+} from "reactstrap";
+import { Link } from "react-router-dom";
+
+class Signup extends Component {
+  render() {
+    return (
+      <div className="signup-container">
+        <Form className="signup-form">
+          <FormGroup>
+            <Label for="exampleEmail">Input without validation</Label>
+            <Input />
+            <FormFeedback>You will not be able to see this</FormFeedback>
+            <FormText>Example help text that remains unchanged.</FormText>
+          </FormGroup>
+          <FormGroup>
+            <Label for="exampleEmail">Valid input</Label>
+            <Input valid />
+            <FormFeedback valid>Sweet! that name is available</FormFeedback>
+            <FormText>Example help text that remains unchanged.</FormText>
+          </FormGroup>
+          <FormGroup>
+            <Label for="examplePassword">Invalid input</Label>
+            <Input invalid />
+            <FormFeedback>Oh noes! that name is already taken</FormFeedback>
+            <FormText>Example help text that remains unchanged.</FormText>
+          </FormGroup>
+          <FormGroup>
+            <Label for="exampleEmail">Input without validation</Label>
+            <Input />
+            <FormFeedback tooltip>
+              You will not be able to see this
+            </FormFeedback>
+            <FormText>Example help text that remains unchanged.</FormText>
+          </FormGroup>
+          <FormGroup>
+            <Label for="exampleEmail">Valid input</Label>
+            <Input valid />
+
+            <FormText>Example help text that remains unchanged.</FormText>
+          </FormGroup>
+          <div className="form-buttons">
+            <Button color="success">Submit</Button>
+           <Link to="/"><Button color="primary">Cancel</Button></Link>
+          </div>
+        </Form>
+      </div>
+    );
+  }
+}
+export default Signup;
