bff0cfef106eab8ed9430bb635d184a8304a43b4
Jacob Leon Lyerla <umasour@yahoo.com>
2018-12-06 00:19:52 -0700

    working on grid layout decided to import in some lower res pictures that would now have to be forced to fit

--- dreamtrekking/src/components/tags.js
+++ dreamtrekking/src/components/tags.js
@@ -2,27 +2,31 @@ import React from "react";
 import { Row, Col, Media } from "reactstrap";
 import desert from "../assets/desert.jpg";
 import woods from "../assets/red-forest.jpg";
-import water from "../assets/water.jpg";
+import water from "../assets/rain.jpeg";
+import snow from "../assets/snow.jpeg"
 const Tags = () => {
   return (
-    <Col md="8" className="tags-container">
+    <Col md="10" className="tags-container">
 
       <Row>
-        <Col className="tag-img" md="3">
-          <Media className="tag" src={desert} />
-        </Col>
-        <Col className="tag-img" md="3">
-          <Media className="tag" src={woods} />
-        </Col>{" "}
-        <Col className="tag-img" md="3">
+        <Col className="tag-img" md="6">
           <Media className="tag" src={water} />
         </Col>
-        <Col className="tag-img" md="3">
+        <Col className="tag-img" md="6">
+          <Media className="tag" src={snow} />
+        </Col>{" "}
+        <Col className="tag-img" md="6">
+          <Media className="tag" src={desert} />
+        </Col>
+        <Col className="tag-img" md="6">
           <Media className="tag" src={woods} />
         </Col>{" "}
-        <Col className="tag-img" md="3">
+        <Col className="tag-img" md="10">
           <Media className="tag" src={woods} />
         </Col>{" "}
+        <Col className="tag-img" md="10">
+          <Media className="tag" src={desert} />
+        </Col>
 
       </Row>
 
