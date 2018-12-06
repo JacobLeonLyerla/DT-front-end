e14897b4bc04a9b4f569d822801b34dfab8b60bd
Jacob Leon Lyerla <umasour@yahoo.com>
2018-12-05 23:57:28 -0700

    still working on figuring out this grid for the pictures

--- dreamtrekking/src/components/tags.js
+++ dreamtrekking/src/components/tags.js
@@ -1,19 +1,31 @@
 import React from "react";
 import { Row, Col, Media } from "reactstrap";
 import desert from "../assets/desert.jpg";
+import woods from "../assets/red-forest.jpg";
+import water from "../assets/water.jpg";
 const Tags = () => {
   return (
-    <Col md="10" className="tags-container">
+    <Col md="8" className="tags-container">
+
       <Row>
         <Col className="tag-img" md="3">
           <Media className="tag" src={desert} />
         </Col>
         <Col className="tag-img" md="3">
-          <Media className="tag" src={desert} />
-        </Col>  <Col className="tag-img" md="3">
-          <Media className="tag" src={desert} />
+          <Media className="tag" src={woods} />
+        </Col>{" "}
+        <Col className="tag-img" md="3">
+          <Media className="tag" src={water} />
         </Col>
+        <Col className="tag-img" md="3">
+          <Media className="tag" src={woods} />
+        </Col>{" "}
+        <Col className="tag-img" md="3">
+          <Media className="tag" src={woods} />
+        </Col>{" "}
+
       </Row>
+
     </Col>
   );
 };
