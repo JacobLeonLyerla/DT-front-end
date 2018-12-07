993ada0f831b35e4f4d13b7c4494a8a88ae7c593
Jacob Leon Lyerla <umasour@yahoo.com>
2018-12-06 23:50:50 -0700

    i do not like the color for this one, but i am going to move on and fix it later, working on the nav bar

--- dreamtrekking/src/components/dashboard.js
+++ dreamtrekking/src/components/dashboard.js
@@ -6,38 +6,49 @@ import {
   NavbarBrand,
   Media,
   UncontrolledDropdown,
+  NavItem,
+  NavLink,
   DropdownToggle,
   DropdownMenu,
   DropdownItem
 } from "reactstrap";
 import { Link } from "react-router-dom";
 import logo from "../assets/logo.png";
-const Dashboard =()=> {
+const Dashboard = () => {
+  return (
+    <Col md="2">
+      <div className="dashboard">
+        <Navbar >
+          <NavbarBrand href="/">
+            <Media src={logo} />
+          </NavbarBrand>
+         
+            <Link to="/userinfo" style={{ textDecoration: 'none',}}>
+            name placeholder
+            </Link>
+            <Link to="/userinfo" style={{ textDecoration: 'none',}}>
+           notifications
+            </Link>
+            <Link to="/userinfo" style={{ textDecoration: 'none',}}>
+            settings
+            </Link>
+          
 
-    return (
-      <Col md="2">
-        <div className="dashboard">
-          <Navbar light expand="md">
-            <NavbarBrand href="/">
-              <Media src={logo} />
-            </NavbarBrand>
-            <NavbarToggler onClick={this.toggle} />
-            <UncontrolledDropdown color="primary">
-      <DropdownToggle caret>
-        Catagories
-      </DropdownToggle>
-      <DropdownMenu  >
-        <DropdownItem >Forest</DropdownItem>
-        <DropdownItem >Lake</DropdownItem>
-        <DropdownItem>Market</DropdownItem>
-    
-        <DropdownItem>Beach</DropdownItem>
-      </DropdownMenu>
-    </UncontrolledDropdown>
-          </Navbar>
-        </div>
-      </Col>
-    );
-  
-}
+          <NavbarToggler onClick={this.toggle} />
+
+          <UncontrolledDropdown color="primary">
+            <DropdownToggle caret>Catagories</DropdownToggle>
+            <DropdownMenu>
+              <DropdownItem>Forest</DropdownItem>
+              <DropdownItem>Lake</DropdownItem>
+              <DropdownItem>Market</DropdownItem>
+
+              <DropdownItem>Beach</DropdownItem>
+            </DropdownMenu>
+          </UncontrolledDropdown>
+        </Navbar>
+      </div>
+    </Col>
+  );
+};
 export default Dashboard;
