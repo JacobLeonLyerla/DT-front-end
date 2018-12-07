c3a94c7ffac2efa951d1c27350d3a5492a356f33
Jacob Leon Lyerla <umasour@yahoo.com>
2018-12-06 23:35:59 -0700

    setting up the dashboard

--- dreamtrekking/src/components/dashboard.js
+++ dreamtrekking/src/components/dashboard.js
@@ -4,20 +4,40 @@ import {
   Navbar,
   NavbarToggler,
   NavbarBrand,
-  Media
-} from 'reactstrap';
-import logo from "../assets/logo.png"
-const Dashboard = () => {
-  return (
-    <Col md="2">
-     <div className="dashboard">
-        <Navbar  light expand="md">
-          <NavbarBrand href="/"><Media src={logo}/></NavbarBrand>
-          <NavbarToggler onClick={this.toggle} />
-         
-        </Navbar>
-      </div>
-    </Col>
-  );
-};
+  Media,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem
+} from "reactstrap";
+import { Link } from "react-router-dom";
+import logo from "../assets/logo.png";
+const Dashboard =()=> {
+
+    return (
+      <Col md="2">
+        <div className="dashboard">
+          <Navbar light expand="md">
+            <NavbarBrand href="/">
+              <Media src={logo} />
+            </NavbarBrand>
+            <NavbarToggler onClick={this.toggle} />
+            <UncontrolledDropdown color="primary">
+      <DropdownToggle caret>
+        Catagories
+      </DropdownToggle>
+      <DropdownMenu  >
+        <DropdownItem >Forest</DropdownItem>
+        <DropdownItem >Lake</DropdownItem>
+        <DropdownItem>Market</DropdownItem>
+    
+        <DropdownItem>Beach</DropdownItem>
+      </DropdownMenu>
+    </UncontrolledDropdown>
+          </Navbar>
+        </div>
+      </Col>
+    );
+  
+}
 export default Dashboard;
