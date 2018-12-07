93a95c9e80c23345509b63c9b8299dd272769e53
Jacob Leon Lyerla <umasour@yahoo.com>
2018-12-06 23:03:08 -0700

    changed my temp nav into a reactstrap nav component

--- dreamtrekking/src/components/dashboard.js
+++ dreamtrekking/src/components/dashboard.js
@@ -1,9 +1,22 @@
 import React from "react";
-import { Col } from "reactstrap";
+import {
+  Col,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Media
+} from 'reactstrap';
+import logo from "../assets/logo.png"
 const Dashboard = () => {
   return (
     <Col md="2">
-      <div className="dashboard" />
+     <div className="dashboard">
+        <Navbar  light expand="md">
+          <NavbarBrand href="/"><Media src={logo}/></NavbarBrand>
+          <NavbarToggler onClick={this.toggle} />
+         
+        </Navbar>
+      </div>
     </Col>
   );
 };
