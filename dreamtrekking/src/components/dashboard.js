a29df8d5d1fed952222bf6527d19ea1301010328
Jacob Leon Lyerla <umasour@yahoo.com>
2018-12-07 18:13:37 -0700

    dashboard working

--- dreamtrekking/src/components/dashboard.js
+++ dreamtrekking/src/components/dashboard.js
@@ -7,13 +7,17 @@ import {
   UncontrolledDropdown,
   DropdownToggle,
   DropdownMenu,
-  DropdownItem
+  DropdownItem,
+  Button
 } from "reactstrap";
 import { Link } from "react-router-dom";
 import logo from "../assets/logo.png";
-const Dashboard = () => {
+const Dashboard = props => {
+  console.log(props)
+  
   return (
-    <Col md="2">
+
+    <Col md={`${1 + props.dashboardVar}`}>
       <div className="dashboard">
         <Navbar >
           <NavbarBrand href="/">
@@ -28,6 +32,7 @@ const Dashboard = () => {
             <Link className="nav-links" to="/userinfo" style={{ textDecoration: 'none',}}>
             settings
             </Link>
+            
           <UncontrolledDropdown color="primary">
             <DropdownToggle caret>Catagories</DropdownToggle>
             <DropdownMenu>
@@ -37,7 +42,7 @@ const Dashboard = () => {
               <DropdownItem>Beach</DropdownItem>
             </DropdownMenu>
           </UncontrolledDropdown>
-        
+        <Button onClick={()=> props.columnSizer()}>tab</Button>
         </Navbar>
       </div>
     </Col>
