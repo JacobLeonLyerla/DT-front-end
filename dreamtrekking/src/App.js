2bbbc52cb244f6c6678449d205d72195fd53a3c6
Jacob Leon Lyerla <umasour@yahoo.com>
2018-12-10 20:56:53 -0700

    added more assets to the program

--- dreamtrekking/src/App.js
+++ dreamtrekking/src/App.js
@@ -14,35 +14,41 @@ import Tags from "./components/tags";
 import logo from "./assets/logo.png";
 import logoSml from "./assets/logoinit.png";
 class App extends Component {
-state={
-  dashboardVar:1,
-  tagVar:0,
-  collapseIcon:"fas fa-angle-double-left",
-  buttonClass:"btn",
-  linkClass:"nav-links",
-  logo:logo,
-
-}
-  columnSizer =()=>{
-    let dv,tv,icon,button,link,newLogo;
-    if(this.state.dashboardVar === 1){
-       dv = this.state.dashboardVar -1
-       tv = this.state.tagVar +1
-       icon = "fas fa-angle-double-right"
-       button = "btn-sml"
-       link= "nav-sml"
-       newLogo =logoSml
-    }else{
-       dv = this.state.dashboardVar +1
-        tv = this.state.tagVar-1
-        icon = "fas fa-angle-double-left"
-        button = "btn"
-        link = "nav-links"
-        newLogo = logo
+  state = {
+    dashboardVar: 1,
+    tagVar: 0,
+    collapseIcon: "fas fa-angle-double-left",
+    buttonClass: "btn",
+    linkClass: "nav-links",
+    logo: logo
+  };
+  columnSizer = () => {
+    let dv, tv, icon, button, link, newLogo;
+    if (this.state.dashboardVar === 1) {
+      dv = this.state.dashboardVar - 1;
+      tv = this.state.tagVar + 1;
+      icon = "fas fa-angle-double-right";
+      button = "btn-sml";
+      link = "nav-sml";
+      newLogo = logoSml;
+    } else {
+      dv = this.state.dashboardVar + 1;
+      tv = this.state.tagVar - 1;
+      icon = "fas fa-angle-double-left";
+      button = "btn";
+      link = "nav-links";
+      newLogo = logo;
     }
-    
-   this.setState({dashboardVar:dv, tagVar:tv,collapseIcon:icon,buttonClass:button,linkClass:link,logo:newLogo})
-  }
+
+    this.setState({
+      dashboardVar: dv,
+      tagVar: tv,
+      collapseIcon: icon,
+      buttonClass: button,
+      linkClass: link,
+      logo: newLogo
+    });
+  };
   render() {
     return (
       <div className="App">
@@ -54,8 +60,24 @@ state={
         <Route path="/signup" render={props => <Signup />} />
         <Route path="/signin" render={props => <Signin />} />
         <Row>
-          <Route path="/dashboard" render={props => <Dashboard logo={this.state.logo}  link={this.state.linkClass} btn={this.state.buttonClass} collapseIcon={this.state.collapseIcon} columnSizer={this.columnSizer} dashboardVar={this.state.dashboardVar} />} />
-          <Route exact path="/dashboard" render={props => <Tags  tagVar={this.state.tagVar}  />} />
+          <Route
+            path="/dashboard"
+            render={props => (
+              <Dashboard
+                logo={this.state.logo}
+                link={this.state.linkClass}
+                btn={this.state.buttonClass}
+                collapseIcon={this.state.collapseIcon}
+                columnSizer={this.columnSizer}
+                dashboardVar={this.state.dashboardVar}
+              />
+            )}
+          />
+          <Route
+            exact
+            path="/dashboard"
+            render={props => <Tags tagVar={this.state.tagVar} />}
+          />
         </Row>
       </div>
     );
