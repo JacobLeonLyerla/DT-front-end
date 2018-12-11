518e8e3e17ee372c75926b950580dfb5b893e5fe
Jacob Leon Lyerla <umasour@yahoo.com>
2018-12-10 20:37:59 -0700

    imported in logo's and sent them based on what one i needed

--- dreamtrekking/src/App.js
+++ dreamtrekking/src/App.js
@@ -11,32 +11,37 @@ import Signup from "./components/signup";
 import Signin from "./components/signin";
 import Dashboard from "./components/dashboard";
 import Tags from "./components/tags";
+import logo from "./assets/logo.png";
+import logoSml from "./assets/logoinit.png";
 class App extends Component {
 state={
   dashboardVar:1,
   tagVar:0,
   collapseIcon:"fas fa-angle-double-left",
   buttonClass:"btn",
-  linkClass:"nav-links"
+  linkClass:"nav-links",
+  logo:logo,
 
 }
   columnSizer =()=>{
-    let dv,tv,icon,button,link;
+    let dv,tv,icon,button,link,newLogo;
     if(this.state.dashboardVar === 1){
        dv = this.state.dashboardVar -1
        tv = this.state.tagVar +1
        icon = "fas fa-angle-double-right"
        button = "btn-sml"
        link= "nav-sml"
+       newLogo =logoSml
     }else{
        dv = this.state.dashboardVar +1
         tv = this.state.tagVar-1
         icon = "fas fa-angle-double-left"
         button = "btn"
         link = "nav-links"
+        newLogo = logo
     }
     
-   this.setState({dashboardVar:dv, tagVar:tv,collapseIcon:icon,buttonClass:button,linkClass:link})
+   this.setState({dashboardVar:dv, tagVar:tv,collapseIcon:icon,buttonClass:button,linkClass:link,logo:newLogo})
   }
   render() {
     return (
@@ -49,7 +54,7 @@ state={
         <Route path="/signup" render={props => <Signup />} />
         <Route path="/signin" render={props => <Signin />} />
         <Row>
-          <Route path="/dashboard" render={props => <Dashboard link={this.state.linkClass} btn={this.state.buttonClass} collapseIcon={this.state.collapseIcon} columnSizer={this.columnSizer} dashboardVar={this.state.dashboardVar} />} />
+          <Route path="/dashboard" render={props => <Dashboard logo={this.state.logo}  link={this.state.linkClass} btn={this.state.buttonClass} collapseIcon={this.state.collapseIcon} columnSizer={this.columnSizer} dashboardVar={this.state.dashboardVar} />} />
           <Route exact path="/dashboard" render={props => <Tags  tagVar={this.state.tagVar}  />} />
         </Row>
       </div>
