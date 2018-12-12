afcd3e200310917bf26a79b85fd0bcd43b280700
Jacob Leon Lyerla <umasour@yahoo.com>
2018-12-11 23:48:48 -0700

    created function to display tags

--- dreamtrekking/src/components/tagView.js
+++ dreamtrekking/src/components/tagView.js
@@ -9,7 +9,7 @@ import { Link } from "react-router-dom";
 
 class TagView extends Component {
 state={
-test:{}
+test:[{tag:""}]
 }
 componentDidMount(){
     this.loadinfo();
@@ -24,40 +24,40 @@ loadinfo = () =>{
       });
 }
 userSubmissionTable= ()=>{
-    return(
+   return  this.state.test.map(sub => 
+   (
     <Table hover>
     <thead>
       <tr>
         <th>#</th>
-        <th>First Name</th>
-        <th>Last Name</th>
-        <th>Username</th>
+        <th>City</th>
+        <th>Country</th>
+        <th>Tags</th>
       </tr>
     </thead>
     <tbody>
       <tr>
         <th scope="row">1</th>
-        <td>Mark</td>
-        <td>Otto</td>
-        <td>@mdo</td>
-      </tr>
-      <tr>
-        <th scope="row">2</th>
-        <td>Jacob</td>
-        <td>Thornton</td>
-        <td>@fat</td>
-      </tr>
-      <tr>
-        <th scope="row">3</th>
-        <td>Larry</td>
-        <td>the Bird</td>
-        <td>@twitter</td>
+        <td>{sub.city}</td>
+        <td>{sub.country}</td>
+        {this.renderTags(sub.tag)}
       </tr>
     </tbody>
-  </Table>
-    )
-}
+  </Table>  
+    
+));
 
+}
+renderTags=(tag)=>{
+    if(tag.length  <1 ){
+        tag = []
+    }
+if (tag !== undefined || tag !==""){
+return tag.map(tag =>(
+    <td className="tag">{`${tag}`}<br/></td>
+))
+}
+}
 render(){
   return (
     <Col md={`${10 + this.props.tagVar}`}>
