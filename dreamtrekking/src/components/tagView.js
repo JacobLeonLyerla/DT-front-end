4ebdd60bf3e0a69a8e84e2bb8eda4fafb428f7e5
Jacob Leon Lyerla <umasour@yahoo.com>
2018-12-11 22:28:58 -0700

    was able to grab data from my db and display it inside of the component. test done

--- dreamtrekking/src/components/tagView.js
+++ dreamtrekking/src/components/tagView.js
@@ -1,17 +1,35 @@
-import React from "react";
+import React,{Component} from "react";
+import axios from "axios";
 import {
   Col,
 
 } from "reactstrap";
 import { Link } from "react-router-dom";
 
-const TagView = props => {
-  console.log(props);
+class TagView extends Component {
+state={
+test:{}
+}
+componentDidMount(){
+    this.loadinfo();
+}
+loadinfo = () =>{
+    axios
+    .get('http://localhost:5500/tags')
+    .then(response => {
+        this.setState({ test: response.data[0] });
+      })
+      .catch(err => {
+      });
+}
 
+render(){
+    console.log(this.state.test.city)
   return (
-    <Col md={`${1 + props.tagVar}`}>
-    
+    <Col md={`${1 + this.props.tagVar}`}>
+    <div>{this.state.test.city}</div>
     </Col>
   );
+}
 };
 export default TagView;
