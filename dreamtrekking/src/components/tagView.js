d917300cdd84b383cbb42ddb31a6d38ed4fba5f4
Jacob Leon Lyerla <umasour@yahoo.com>
2018-12-13 23:28:51 -0700

    pulled data from my pictures collection set up axios call

--- dreamtrekking/src/components/tagView.js
+++ dreamtrekking/src/components/tagView.js
@@ -9,6 +9,7 @@ class TagView extends Component {
     test: [{ tag: "" }],
     tag: "",
     flag:false,
+    picture:[],
   };
   componentDidMount() {
     let { id } = this.props.match.params;
@@ -31,6 +32,23 @@ class TagView extends Component {
           });
 
         this.setState({ test:tagArr });
+      })
+      .catch(err => {});
+      axios
+      .get("http://localhost:5500/pictures")
+      .then(response => {
+        console.log(response.data)
+        let pictureArr =[];
+          response.data.forEach(e => {
+            for (let i = 0; i < e.tag.length; i++) {
+              if(e.tag[i].toLowerCase() === this.state.tag.toLowerCase()){
+               pictureArr.push(e)
+              }
+              
+            }
+          });
+
+        this.setState({ picture:pictureArr });
       })
       .catch(err => {});
   };
@@ -92,6 +110,21 @@ class TagView extends Component {
       
     }
   }
+  renderRelatedImgs=()=>{
+
+    return (
+    <Row className="tag-filtered">
+    <Col data-ca="ca" className="tag-img water" md="6">
+      <Link onClick={()=>this.checkp()} to="/dashboard/water" style={{ textDecoration: "none" }}>
+        <div onClick={()=>this.checkp()} className="cover ">
+          {" "}
+          <p>Lake</p>
+        </div>
+      </Link>
+    </Col>
+  </Row>
+  )
+  }
   render() {
     
     return (
@@ -102,89 +135,8 @@ class TagView extends Component {
           <p>Google Placeholder</p>
         </div>
         {this.renderTable()}
-
-        <Row className="tag-filtered">
-          <Col data-ca="ca" className="tag-img water" md="6">
-            <Link onClick={()=>this.checkp()} to="/dashboard/water" style={{ textDecoration: "none" }}>
-              <div onClick={()=>this.checkp()} className="cover ">
-                {" "}
-                <p>Lake</p>
-              </div>
-            </Link>
-          </Col>
-          <Col className="tag-img snow" md="6">
-            <Link to="/dashboard/snow" style={{ textDecoration: "none" }}>
-              <div className="cover ">
-                {" "}
-                <p>Snow</p>
-              </div>
-            </Link>
-          </Col>{" "}
-          <Col className="tag-img market" md="6">
-            <Link to="/dashboard/market" style={{ textDecoration: "none" }}>
-              <div className="cover ">
-                {" "}
-                <p>Market</p>
-              </div>
-            </Link>
-          </Col>
-          <Col className="tag-img camping" md="6">
-            <Link to="/dashboard/camping" style={{ textDecoration: "none" }}>
-              <div className="cover ">
-                {" "}
-                <p>Camping</p>
-              </div>
-            </Link>
-          </Col>
-          <Col className="tag-img ranch" md="6">
-            <Link to="/dashboard/ranch" style={{ textDecoration: "none" }}>
-              <div className="cover ">
-                {" "}
-                <p>Ranch</p>
-              </div>
-            </Link>
-          </Col>
-          <Col className="tag-img asia" md="6">
-            <Link onClick={()=>this.checkp()} to="/dashboard/asia" style={{ textDecoration: "none" }}>
-              <div className="cover ">
-                {" "}
-                <p>Asia</p>
-              </div>
-            </Link>
-          </Col>
-          <Col className="tag-img europe" md="6">
-            <Link onClick={()=>this.checkp()} to="/dashboard/europe" style={{ textDecoration: "none" }}>
-              <div className="cover ">
-                {" "}
-                <p>Europe</p>
-              </div>
-            </Link>
-          </Col>
-          <Col className="tag-img africa" md="6">
-            <Link to="/dashboard/africa" style={{ textDecoration: "none" }}>
-              <div className="cover ">
-                {" "}
-                <p>Africa</p>
-              </div>
-            </Link>
-          </Col>
-          <Col className="tag-img alaska" md="6">
-            <Link to="/dashboard/alaska" style={{ textDecoration: "none" }}>
-              <div className="cover ">
-                {" "}
-                <p>Alaska</p>
-              </div>
-            </Link>
-          </Col>
-          <Col className="tag-img nz" md="6">
-            <Link to="/dashboard/nz" style={{ textDecoration: "none" }}>
-              <div className="cover ">
-                {" "}
-                <p>New Zeland</p>
-              </div>
-            </Link>
-          </Col>
-        </Row>
+        {this.renderRelatedImgs()}
+      
       </Col>
     );
   }
