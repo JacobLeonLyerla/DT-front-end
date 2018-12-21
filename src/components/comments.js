7aefa8a4740e75cc3a85c220931f980417bf9aa2
Jacob Leon Lyerla <umasour@yahoo.com>
2018-12-21 02:08:12 -0700

    set up a basic input

--- src/components/comments.js
+++ src/components/comments.js
@@ -3,14 +3,16 @@
 
 
 import React, { Component,Fragment } from "react";
-import {Input, Row, Col } from "reactstrap";
+import {Input,Form, Row, Col } from "reactstrap";
 import { Link } from "react-router-dom";
 import axios from "axios";
 
 class Comments extends Component {
- 
+ state={
+     comment:"",
+ }
     renderComments=()=>{
-        if(this.props.comments !== undefined){}
+        if(this.props.comments !== undefined){
         if(this.props.comments.length >0){
             return this.props.comments.map(comment=>{
                 <div>{comment}</div>
@@ -19,13 +21,21 @@ class Comments extends Component {
             return<div className="no-comments"></div>
         }
     }
-
+}
+handleInput = input => {
+    this.setState({ [input.target.name]: input.target.value });
+  };
   render() {
     return (<Fragment>
         {this.renderComments()}
+        <Form>
         <Input
-        style={{height:"20vh"}}
-        />
+        name="comment"
+        value={this.state.comment}
+        onchange={this.handleInput}
+        type="textarea"
+        style={{height:"10vh"}}
+        /></Form>
   </Fragment>);
   }
 }
