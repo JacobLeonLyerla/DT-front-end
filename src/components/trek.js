ad3e7e8b1ad43ab668b25d9abe2293614492d86c
Jacob Leon Lyerla <umasour@yahoo.com>
2018-12-20 23:23:01 -0700

    setting up a basic tag component i think i am going to take a break tho.

--- src/components/trek.js
+++ src/components/trek.js
@@ -1,15 +1,36 @@
 import React,{Component} from "react";
 import { Row, Col } from "reactstrap";
 import {Link} from "react-router-dom"
-
+import axios from "axios";
+import Pictures from "./renderPictures";
 
 class Trek extends Component{
+state={
+    tag:"",
+}
+componentDidMount(){
+    let {id} =this.props.match.params
+   
+    axios
+    .get(`https://dt-back-end.herokuapp.com/tags/${id}`)
+    .then(response =>{
+        console.log(response.data)
+        this.setState({tag:response.data})
+    }).catch(err=>{
+        console.log(err)
+    })
+}
 
     render(){
 
         return(
-          
-<div>HI</div>
+            <Col className="table-container" md={`${10 + this.props.tagVar}`}>
+<div>{this.state.tag.name}<br/>
+<br/>
+<br/>
+{this.state.tag.description}</div>
+<Pictures imgTag={this.state.tag.tag} style="tag-filtered"/>
+</Col>
         )
     }
 }
