4a768c4435484e425094d3858bc34ce9497a897c
Jacob Leon Lyerla <umasour@yahoo.com>
2018-12-20 23:43:42 -0700

    basic boilerplate

--- src/components/comments.js
+++ src/components/comments.js
@@ -1,1 +1,21 @@
 // here is where we will render the commetns and set up all the puts and stuff to maintain them
+
+
+
+import React, { Component } from "react";
+import { Row, Col } from "reactstrap";
+import { Link } from "react-router-dom";
+import axios from "axios";
+
+class Comments extends Component {
+ 
+
+  render() {
+    return (
+      <Col className="table-container" md={`${10 + this.props.tagVar}`}>
+     
+      </Col>
+    );
+  }
+}
+export default Comments;
