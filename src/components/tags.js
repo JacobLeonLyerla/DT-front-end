3a5a2c25740134ac8b8c3ce4486b5eb76a4382a5
Jacob Leon Lyerla <umasour@yahoo.com>
2018-12-20 22:54:54 -0700

    changed out the function and instead render a component that does the same thing

--- src/components/tags.js
+++ src/components/tags.js
@@ -2,31 +2,15 @@ import React,{Component} from "react";
 import { Row, Col } from "reactstrap";
 import {Link} from "react-router-dom"
 import imgs from "../assets/exportImgs.js" 
+
+import Pictures from "./renderPictures.js"
 class Tags extends Component {
 
 
-  renderTags =()=>{
-    if(this.props.pictures.length >0){
-      return this.props.pictures.map(img =>{
-   
-      return(<Col data-ca="ca" style={{backgroundImage : `url(${imgs[img.name.replace(/-/g, '').toLowerCase()]})`}} className={`tag-img tags`} md="6">
-       <Link to={`/dashboard/${img.name.toLowerCase()}`} style={{ textDecoration: "none" }}>
-          <div className="cover ">
-            {" "}
-            <p>{img.name.replace(/-/g, ' ')}</p>
-          </div></Link>
-        </Col>
-        )})
-  }else{
-
-  }
-}
   render(){
   return (
     <Col md={`${10 + this.props.tagVar}`} className="tags-container">
-      <Row>
-      {this.renderTags()}
-      </Row>
+      <Pictures pictures={this.props.pictures}/>
     </Col>
   );
   }
