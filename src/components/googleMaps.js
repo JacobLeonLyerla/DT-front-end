9496cbb62b1887d695e301912536b5b1153b46bd
Jacob Leon Lyerla <umasour@yahoo.com>
2018-12-20 23:32:52 -0700

    built out the new maps component placeholder and rendered it inside of trek

---
+++ src/components/googleMaps.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+class Maps extends Component {
+  render() {
+    return (
+      <div className="google">
+        <p>Google Placeholder</p>
+      </div>
+    );
+  }
+}
+export default Maps;
--- src/components/trek.js
+++ src/components/trek.js
@@ -1,37 +1,41 @@
-import React,{Component} from "react";
+import React, { Component } from "react";
 import { Row, Col } from "reactstrap";
-import {Link} from "react-router-dom"
+import { Link } from "react-router-dom";
 import axios from "axios";
 import Pictures from "./renderPictures";
+import Map from "./googleMaps";
+class Trek extends Component {
+  state = {
+    tag: ""
+  };
+  componentDidMount() {
+    let { id } = this.props.match.params;
 
-class Trek extends Component{
-state={
-    tag:"",
-}
-componentDidMount(){
-    let {id} =this.props.match.params
-   
     axios
-    .get(`https://dt-back-end.herokuapp.com/tags/${id}`)
-    .then(response =>{
-        console.log(response.data)
-        this.setState({tag:response.data})
-    }).catch(err=>{
-        console.log(err)
-    })
-}
+      .get(`https://dt-back-end.herokuapp.com/tags/${id}`)
+      .then(response => {
+        console.log(response.data);
+        this.setState({ tag: response.data });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
 
-    render(){
-
-        return(
-            <Col className="table-container" md={`${10 + this.props.tagVar}`}>
-<div>{this.state.tag.name}<br/>
-<br/>
-<br/>
-{this.state.tag.description}</div>
-<Pictures imgTag={this.state.tag.tag} style="tag-filtered"/>
-</Col>
-        )
-    }
+  render() {
+    return (
+      <Col className="table-container" md={`${10 + this.props.tagVar}`}>
+        <Map />
+        <div>
+          {this.state.tag.name}
+          <br />
+          <br />
+          <br />
+          {this.state.tag.description}
+        </div>
+        <Pictures imgTag={this.state.tag.tag} style="tag-filtered" />
+      </Col>
+    );
+  }
 }
 export default Trek;
