f2a6c777c56be31ee3d5524ca75e7f73f4a9ac88
Jacob Leon Lyerla <umasour@yahoo.com>
2018-12-21 21:00:08 -0700

    imported reply component and rendered it passing in the id

--- src/components/comments.js
+++ src/components/comments.js
@@ -5,7 +5,7 @@ import { Button, Input, Form, Row, Col } from "reactstrap";
 import { Link } from "react-router-dom";
 import axios from "axios";
 import Edit from "./edit"
-
+import Reply from "./reply"
 class Comments extends Component {
   state = { comment: "" };
 
@@ -13,7 +13,7 @@ class Comments extends Component {
     if (this.props.comments !== undefined) {
       if (this.props.comments.length > 0) {
         return this.props.comments.map(comment => {
-          return <div className="comment-container">{(comment.username !== this.props.user.username) ?(<Fragment><div className="target-comment"><i class="fas fa-reply"></i></div><div className="username">{comment.username}</div></Fragment>):(<Fragment><Edit id={comment._id}/><div className="username">You Commented</div></Fragment>)}<div className="comment">{comment.comment}</div></div>;
+          return <div className="comment-container">{(comment.username !== this.props.user.username) ?(<Fragment><Reply id={comment._id}/><div className="username">{comment.username}</div></Fragment>):(<Fragment><Edit id={comment._id}/><div className="username">You Commented</div></Fragment>)}<div className="comment">{comment.comment}</div></div>;
         });
       } else {
         return <div className="no-comments">No comments yet</div>;
