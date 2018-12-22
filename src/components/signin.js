64cc1a9701b7b09a498c742b8082969a235f3d6f
Jacob Leon Lyerla <umasour@yahoo.com>
2018-12-21 23:11:48 -0700

    trying to purge localstorage

--- src/components/signin.js
+++ src/components/signin.js
@@ -25,6 +25,9 @@ class Signup extends Component {
     };
     this.handleChange = this.handleChange.bind(this);
   }
+  componentDidMount(){
+    localStorage.clear()
+  }
   validateEmail(e) {
     const emailRex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     const { validate } = this.state;
