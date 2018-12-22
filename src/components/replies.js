39298c1944588aded1bf257cbd223a4a0f824008
Jacob Leon Lyerla <umasour@yahoo.com>
2018-12-22 02:52:28 -0700

    rendering replies

--- src/components/replies.js
+++ src/components/replies.js
@@ -1,19 +1,39 @@
 import React,{Component,Fragment} from 'react';
 
  class Replies extends Component {
+    state={
+        reply:{}
+    }
+    componentDidMount(){
+        this.setupReplies()
+    }
+    setupReplies=()=>{
+           axios
+           .get(`https://dt-back-end.herokuapp.com/comments/${this.props.replies}`)
+           .then(response=>{
+           console.log(response.data)
+         
+           }).catch(err=>{
+        
+           })
+           return data
+          
+    }
 
        renderReplies=()=>{
            if(this.props.replies !== undefined){
             return this.props.replies.map(reply =>(<Fragment>
-       
-       <div className="reply"> <div className="reply-username">{reply.username}</div>
-            <div className="reply-comment">{reply.replies}</div></div>
+               
+        <div className="reply">{reply.username === this.props.user.username ?(<Fragment>aaa</Fragment>):(<Fragment>bbb</Fragment>)}
+        <div className="reply-username">{reply.username}</div>
+            <div className="reply-comment">{reply.Comment}</div></div>
 
              </Fragment>))
 
            }
        }
   render() {
+  
     return (
         <Fragment>
         
