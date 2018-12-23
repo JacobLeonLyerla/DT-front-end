123e4f19cb9cfe3878b44e0ffe8139e9cd5bd8b5
Jacob Leon Lyerla <umasour@yahoo.com>
2018-12-22 18:01:02 -0700

    slight cleanup and built out basic boiler for subreplies

--- src/components/edit.js
+++ src/components/edit.js
@@ -1,35 +1,49 @@
-import React from 'react';
-import { Tooltip } from 'reactstrap';
- class Edit extends React.Component {
-    constructor(props) {
-        super(props);
-    
-        this.toggle = this.toggle.bind(this);
-        this.state = {
-          tooltipEdit: false,
-          tooltipDelete:false
-        };
-      }
-    
-      toggle(type) {
-          if(type ==="edit"){
-        this.setState({
-          tooltipEdit: !this.state.tooltipEdit,
-         
-        });}else{
-        this.setState({
-          tooltipDelete:!this.state.tooltipDelete
-           
-          }); 
-      }}
+import React from "react";
+import { Tooltip } from "reactstrap";
+class Edit extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      tooltipEdit: false,
+      tooltipDelete: false
+    };
+  }
+
+  toggle(type) {
+    if (type === "edit") {
+      this.setState({
+        tooltipEdit: !this.state.tooltipEdit
+      });
+    } else {
+      this.setState({
+        tooltipDelete: !this.state.tooltipDelete
+      });
+    }
+  }
   render() {
     return (
-     <div className="target-comment"><i id={`edit${this.props.id}`} class="fas fa-edit"></i>  <Tooltip placement="left" isOpen={this.state.tooltipEdit} target={`edit${this.props.id}`} toggle={()=>this.toggle("edit")}>
-     Edit
-   </Tooltip>|<i id={`delete${this.props.id}`} class="fas fa-times"></i>
-   <Tooltip placement="right" isOpen={this.state.tooltipDelete} target={`delete${this.props.id}`} toggle={this.toggle}>
-     delete
-   </Tooltip></div>  
+      <div className="target-comment">
+        <i id={`edit${this.props.id}`} class="fas fa-edit" />{" "}
+        <Tooltip
+          placement="left"
+          isOpen={this.state.tooltipEdit}
+          target={`edit${this.props.id}`}
+          toggle={() => this.toggle("edit")}
+        >
+          Edit
+        </Tooltip>
+        |<i id={`delete${this.props.id}`} class="fas fa-times" />
+        <Tooltip
+          placement="right"
+          isOpen={this.state.tooltipDelete}
+          target={`delete${this.props.id}`}
+          toggle={this.toggle}
+        >
+          delete
+        </Tooltip>
+      </div>
     );
   }
 }
---
+++ src/components/subReplies.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+class SubReply  extends Component {
+  render() {
+    return (
+      <div >
+    
+      </div>
+    );
+  }
+}
+export default SubReply;
