5a62b90b4f4acc494add7fcedae3fc827e7849e2
Jacob Leon Lyerla <umasour@yahoo.com>
2018-12-22 23:31:58 -0700

    delete component is working fine

--- src/components/delete.js
+++ src/components/delete.js
@@ -1,14 +1,53 @@
-import React,{Component} from "react";
-import { Tooltip } from "reactstrap";
+import React,{Component,Fragment} from "react";
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
+
 import axios from "axios";
 class Delete extends Component {
-  
-  render() {
-    return (
+    constructor(props) {
+        super(props);
+        this.state = {
+          modal: false
+        };
+    
+        this.toggle = this.toggle.bind(this);
+      }
+    
+      toggle() {
+        this.setState({
+          modal: !this.state.modal
+        });
+      }
+      handleDelete=()=>{
+          console.log(this.props.id)
+        axios
+        .delete(`https://dt-back-end.herokuapp.com/comments/${this.props.id}`)
+        .then(response => {
+         console.log(response.data)
+         this.toggle()
+      this.props.setupComments(this.props.propsId)
+        })
+        .catch(err => {
+            console.log(err)
+        });
+      }
 
-        <i  class="fas fa-times" />
-       
-    );
+      renderModal=()=>{
+          return(<Fragment>
+      <i  class="fas fa-times"  onClick={this.toggle}/>
+        <Modal isOpen={this.state.modal} toggle={this.toggle} className={this.props.className}>
+  
+          <ModalFooter>
+            <Button color="danger" onClick={()=>this.handleDelete()}>Are you sure you want to delete this?</Button>{' '}
+          </ModalFooter>
+        </Modal>
+
+         </Fragment> )
+      }
+  render() {
+    return (<Fragment>
+{this.renderModal()}
+  
+   </Fragment> );
   }
 }
 export default Delete;
