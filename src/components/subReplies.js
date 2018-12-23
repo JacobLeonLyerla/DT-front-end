a1c930b5b6dbb28bf47737653f47bc11457ba25d
Jacob Leon Lyerla <umasour@yahoo.com>
2018-12-22 19:04:07 -0700

    rendering sub replies but i need to get them refreashing

--- src/components/subReplies.js
+++ src/components/subReplies.js
@@ -1,10 +1,21 @@
-import React, { Component } from "react";
+import React, { Component,Fragment } from "react";
 class SubReply  extends Component {
+
+     renderReplies(){
+         if(this.props.replies !== undefined){
+            return this.props.replies.map(reply=>(<Fragment>
+                  <div className="reply sub-reply"> <div className="reply-username sub-username">{reply.username}</div>
+            <div className="reply-comment sub-comment">{reply.comment}</div></div>
+            </Fragment>)) 
+         }
+      }
+
   render() {
-    return (
-      <div >
-    
-      </div>
+console.log(this.props.replies.length)
+    return (<Fragment>
+
+        {this.renderReplies()}
+    </Fragment>
     );
   }
 }
