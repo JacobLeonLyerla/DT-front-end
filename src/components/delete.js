4416c27ccaca800d275fd3dfb4e94172d4188b9b
Jacob Leon Lyerla <umasour@yahoo.com>
2018-12-22 22:01:14 -0700

    setting up a delete component

---
+++ src/components/delete.js
@@ -0,0 +1,14 @@
+import React,{Component} from "react";
+import { Tooltip } from "reactstrap";
+import axios from "axios";
+class Delete extends Component {
+  
+  render() {
+    return (
+
+        <i  class="fas fa-times" />
+       
+    );
+  }
+}
+export default Delete;
