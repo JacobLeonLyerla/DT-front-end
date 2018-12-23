fcd1d578237ae127c2a1c5bbb7a168f74f2c1add
Jacob Leon Lyerla <umasour@yahoo.com>
2018-12-22 23:32:16 -0700

    edit component is working  i still need to test them on replies to replies

--- src/components/edit.js
+++ src/components/edit.js
@@ -1,7 +1,8 @@
-import React from "react";
+import React,{Component} from "react";
 import { Tooltip } from "reactstrap";
 import Delete from "./delete"
-class Edit extends React.Component {
+import EditComment from "./editComment"
+class Edit extends Component {
   constructor(props) {
     super(props);
 
@@ -26,24 +27,31 @@ class Edit extends React.Component {
   render() {
     return (
       <div className="target-comment">
-        <i id={`edit${this.props.id}`} class="fas fa-edit" />{" "}
+      <div className="edit-constainer" id={`edit${this.props.id}`}>
+        <EditComment
+        id={this.props.id} comment={this.props.comment} setupComments={this.props.setupComments} propsId={this.props.propsId}
+        replyflag={this.props.replyflag}/>
+        </div>
         <Tooltip
           placement="left"
           isOpen={this.state.tooltipEdit}
           target={`edit${this.props.id}`}
           toggle={() => this.toggle("edit")}
+          
         >
           Edit
         </Tooltip>|
         <div className="delete-constainer" id={`delete${this.props.id}`}>
         <Delete
-       
+       id={this.props.id} comment={this.props.comment} setupComments={this.props.setupComments} propsId={this.props.propsId}
+       replyflag={this.props.replyflag}
         /> </div>
         <Tooltip
           placement="right"
           isOpen={this.state.tooltipDelete}
           target={`delete${this.props.id}`}
           toggle={this.toggle}
+
         >
           delete
         </Tooltip>
