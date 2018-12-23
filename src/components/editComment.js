cf2c70fe51ece67f2f27d19fa015bbbea53929a1
Jacob Leon Lyerla <umasour@yahoo.com>
2018-12-22 23:32:36 -0700

    set up edit comment and tested it, it works perfectly so far

---
+++ src/components/editComment.js
@@ -0,0 +1,70 @@
+import React,{Component,Fragment} from "react";
+import {Form,Input,Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
+
+import axios from "axios";
+class EditComment extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+          modal: false,
+          comment:`${this.props.comment.comment}`
+        };
+    
+        this.toggle = this.toggle.bind(this);
+      }
+    
+      toggle() {
+        this.setState({
+          modal: !this.state.modal
+        });
+      }
+      handleEditComment=()=>{
+          console.log(this.props.id)
+             let edit ={}
+      edit.comment = this.state.comment
+        axios
+        .put(`https://dt-back-end.herokuapp.com/comments/${this.props.id}`, edit)
+        .then(response => {
+         console.log(response.data)
+         this.toggle()
+      this.props.setupComments(this.props.propsId)
+        })
+        .catch(err => {
+            console.log(err)
+        });
+      }
+
+      handleInput = input => {
+        this.setState({ [input.target.name]: input.target.value });
+      };
+      renderModal=()=>{
+          return(<Fragment>
+      <i  class="fas fa-edit"  onClick={this.toggle}/>
+        <Modal isOpen={this.state.modal} toggle={this.toggle} className={this.props.className}>
+    
+        <Form>
+          <Input
+            style={{ height: "30vh" }}
+            name="comment"
+            value={this.state.comment}
+            onChange={this.handleInput}
+            type="textarea"
+          />
+        </Form>
+      
+          <ModalFooter>
+            <Button color="danger" onClick={()=>this.handleEditComment()}>Edit</Button>{' '}
+          </ModalFooter>
+        </Modal>
+
+         </Fragment> )
+      }
+  render() {
+    console.log(this.props.comment.comment)
+    return (<Fragment>
+{this.renderModal()}
+  
+   </Fragment> );
+  }
+}
+export default EditComment;
