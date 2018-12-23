cc24772181aeff4cc33295dcf841d6a6de3d2683
Jacob Leon Lyerla <umasour@yahoo.com>
2018-12-22 18:01:20 -0700

    still trying to render replies to reply

--- src/components/replies.js
+++ src/components/replies.js
@@ -1,14 +1,16 @@
 import React,{Component,Fragment} from 'react';
 import axios from "axios";
-
+import Reply from "./reply"
  class Replies extends Component {
     state={
-        reply:{}
+        reply:{},
+        subReply:{},
+        subReplycalled:false,
     }
 componentDidMount(){
     this.setupReplies()
 }
-    setupReplies=(id)=>{
+    setupReplies=()=>{
         
            axios
            .get(`https://dt-back-end.herokuapp.com/comments/${this.props.id}`)
@@ -21,7 +23,7 @@ componentDidMount(){
     }
 
        renderReplies=()=>{
-          if(this.state.reply !== {}){
+          if(this.state.reply.length !== this.props.replies.length){
               this.setupReplies()
           }
            if(this.props.replies !== undefined){
@@ -30,20 +32,56 @@ componentDidMount(){
            if(this.state.reply.length >0){
             return this.state.reply.map(reply =>(<Fragment>
                
-        <div className="reply">{reply.username === this.props.user.username ?(<Fragment>aaa</Fragment>):(<Fragment>bbb</Fragment>)}
+        <div className="reply">{reply.username === this.props.user.username ?(<Fragment>aaa</Fragment>):(<Fragment> <Reply
+                    reply={reply.replyTo}
+                    id={reply._id}
+                    username={reply.username}
+                    replies={reply.replies}
+                    user={this.props.user}
+                    propsId={this.props.tagId}
+                    setupComments={this.props.setupComments}
+                  /></Fragment>)}
         <div className="reply-username">{reply.username}</div>
             <div className="reply-comment">{reply.comment}</div></div>
+
 
              </Fragment>))
 
            }
         }
        }
+
+       setupSubReplies=(id)=>{
+        axios
+        .get(`https://dt-back-end.herokuapp.com/comments/${id}`)
+        .then(response=>{
+         this.setState({subReply:response.data.replies})
+        }).catch(err=>{
+     
+        })
+    }
+// renderSubReplies=()=>{
+//     if(this.state.subReply !== undefined && this.state.reply !==undefined){
+//     if(this.state.subReply.length !== this.state.reply.replies.length){
+
+//     }}
+//   if(this.state.subReplycalled !==true){
+//    if(this.state.reply.length >0){
+//      this.state.reply.forEach(reply => {
+//          for (let i = 0; i < reply.replies.length; i++) {
+//              this.setupSubReplies(reply.replies[i])
+             
+//          }
+//          this.setupReplies(reply._id)
+         
+//      });
+//    } 
+//   }
+// }
   render() {
     return (
         <Fragment>
         {this.renderReplies()}
-
         </Fragment>)
 
 
