2018255f0e9f4d4874ed71fde0d8bc5959777d05
Jacob Leon Lyerla <umasour@yahoo.com>
2018-12-23 21:04:27 -0700

    setting up the divs for details a bit better

--- src/components/tagDetails.js
+++ src/components/tagDetails.js
@@ -3,8 +3,12 @@ class Details extends Component {
   render() {
     return (
       <div className="details">
-        <div className="username">{this.props.tag.user}</div>
-        <div className="name">{this.props.tag.name}</div>
+        <div className="header">
+          <div className="username">
+            Posted by  <div className="user">{` ${this.props.tag.user}`}</div>
+          </div>
+          <div className="name">{this.props.tag.name}</div>
+        </div>
         <div className="description">{this.props.tag.description}</div>
       </div>
     );
