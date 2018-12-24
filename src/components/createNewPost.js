862a0db320580f69b434364dba22622207f18fe2
Jacob Leon Lyerla <umasour@yahoo.com>
2018-12-23 21:29:38 -0700

    settting up post component and testing it's render

--- src/components/createNewPost.js
+++ src/components/createNewPost.js
@@ -1,8 +1,9 @@
 import React, { Component,Fragment } from "react";
+import {Button} from 'reactstrap';
 class Post extends Component {
   render() {
     return (<Fragment>
-
+<Button>New Post</Button>
 
     </Fragment>);
   }
--- src/components/tagDetails.js
+++ src/components/tagDetails.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+
 class Details extends Component {
   render() {
     return (
