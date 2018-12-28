d6d111b9d1e02ec53c724a2bd7dc261cb21fa656
Jacob Leon Lyerla <umasour@yahoo.com>
2018-12-28 00:09:18 -0700

    likes and comments clearing when you view

--- src/components/post.js
+++ src/components/post.js
@@ -1,27 +1,39 @@
 import React, { Component, Fragment } from "react";
 import { Col, Table } from "reactstrap";
+import axios from "axios";
 import { Link } from "react-router-dom";
 class Post extends Component {
   renderPost = () => {
     let arr = this.sortPost();
     return arr.map(post => (
-     
-       <tr onClick={()=>this.renderRoute(post._id)}
-          className={
-            post.unreadComment + post.unreadLike > 0 ? "tr notify" : "tr no-notify"
-          }
-        >
-          <td >{post.unreadComment + post.unreadLike}</td>
-          <td>{post.name}</td>
-          <td>{post.city}</td>
-          {this.renderTags(post.tag)}
-        </tr>
-     
+      <tr
+        onClick={() => this.renderRoute(post._id)}
+        className={
+          post.unreadComment + post.unreadLike > 0
+            ? "tr notify"
+            : "tr no-notify"
+        }
+      >
+        <td>{post.unreadComment + post.unreadLike}</td>
+        <td>{post.name}</td>
+        <td>{post.city}</td>
+        {this.renderTags(post.tag)}
+      </tr>
     ));
   };
-renderRoute =(id)=>{
-this.props.history.push(`/dashboard/trek/${id}`)
-}
+  renderRoute = id => {
+      let reset = {}
+      reset.unreadComment =0
+      reset.unreadLike =0
+
+      axios
+      .put(`https://dt-back-end.herokuapp.com/tags/${id}`,reset)
+      .then(response => {
+       console.log(response.data)
+      })
+      .catch(err => {});
+    this.props.history.push(`/dashboard/trek/${id}`);
+  };
   renderTags = tag => {
     if (tag.length < 1) {
       tag = [];
@@ -42,21 +54,22 @@ this.props.history.push(`/dashboard/trek/${id}`)
   sortPost = () => {
     let notifyArr = [];
     let oldArr = [];
-    if(this.props.post !== undefined){
-    this.props.post.forEach(post => {
-      if (post.unreadComment + post.unreadLike > 0) {
-        notifyArr.push(post);
-      } else {
-        oldArr.push(post);
-      }
-    });
-    let arr = [...notifyArr, ...oldArr];
-    return arr;
-  }};
+    if (this.props.post !== undefined) {
+      this.props.post.forEach(post => {
+        if (post.unreadComment + post.unreadLike > 0) {
+          notifyArr.push(post);
+        } else {
+          oldArr.push(post);
+        }
+      });
+      let arr = [...notifyArr, ...oldArr];
+      return arr;
+    }
+  };
   render() {
     return (
       <Col className="table-container" md={`${10 + this.props.tagVar}`}>
-        <Table >
+        <Table>
           <thead>
             <tr>
               <th>Notifications</th>
