220cacbcc1b767cdaa7598bb215f402541439d19
Jacob Leon Lyerla <umasour@yahoo.com>
2018-12-27 23:51:36 -0700

    trying to fix some weird issues

--- src/components/post.js
+++ src/components/post.js
@@ -1,71 +1,70 @@
-import React, { Component,Fragment } from "react";
-import {Col,Table} from "reactstrap"
+import React, { Component, Fragment } from "react";
+import { Col, Table } from "reactstrap";
+import { Link } from "react-router-dom";
 class Post extends Component {
-renderPost=()=>{
-        let arr = this.sortPost()
-        console.log(arr)
-          return arr.map(post =>(<Fragment>
-         <tr className={(post.unreadComment+ post.unreadLike >0)?"notify":"no-notify"}>
-             {console.log(post)}
-             <td >{post.unreadComment + post.unreadLike}</td>
-            <td>{post.name}</td>
-            <td>{post.city}</td>
-            {this.renderTags(post.tag)}</tr>
-          </Fragment>))
-      }
+  renderPost = () => {
+    let arr = this.sortPost();
+    return arr.map(post => (
+     
+       <tr
+          className={
+            post.unreadComment + post.unreadLike > 0 ? "tr notify" : "tr no-notify"
+          }
+        >
+          <Link className="link-style"  to={`/dashboard/trek/${post._id}`}> <td>{post.unreadComment + post.unreadLike}</td>
+          <td>{post.name}</td>
+          <td>{post.city}</td>
+          {this.renderTags(post.tag)}</Link>
+        </tr>
+     
+    ));
+  };
 
-      renderTags = tag => {
-         
-        if (tag.length < 1) {
-          tag = [];
-        }
-        if (tag !== undefined || tag !== "") {
-          return (
-            <td className="tag-container">
-              {tag.map(tag => (
-                <div className="tag">
-                  {`${tag.replace(/-/g, " ")}`}
-                  <br />
-                </div>
-              ))}
-            </td>
-          );
-        }
-      };
-sortPost =()=>{
-    let notifyArr=[]
-    let oldArr =[]
+  renderTags = tag => {
+    if (tag.length < 1) {
+      tag = [];
+    }
+    if (tag !== undefined || tag !== "") {
+      return (
+        <td className="tag-container">
+          {tag.map(tag => (
+            <div className="tag">
+              {`${tag.replace(/-/g, " ")}`}
+              <br />
+            </div>
+          ))}
+        </td>
+      );
+    }
+  };
+  sortPost = () => {
+    let notifyArr = [];
+    let oldArr = [];
+    if(this.props.post !== undefined){
     this.props.post.forEach(post => {
-    
-        if((post.unreadComment + post.unreadLike) >0){
-            notifyArr.push(post)
-        }else{
-            oldArr.push(post)
-        }
-        
+      if (post.unreadComment + post.unreadLike > 0) {
+        notifyArr.push(post);
+      } else {
+        oldArr.push(post);
+      }
     });
-     let arr = [...notifyArr, ...oldArr]
-     return arr
-}
+    let arr = [...notifyArr, ...oldArr];
+    return arr;
+  }};
   render() {
-      
     return (
-        <Col className="table-container" md={`${10 + this.props.tagVar}`}>
-  <Table hover>
-        <thead>
-          <tr>
-            <th>Notifications</th>
-            <th>First Name</th>
-            <th>Last Name</th>
-            <th>Username</th>
-          </tr>
-        </thead>
-        <tbody>
-         
-              {this.renderPost()}         
-        </tbody>
-      </Table>
-        
+      <Col className="table-container" md={`${10 + this.props.tagVar}`}>
+        <Table >
+          <thead>
+            <tr>
+              <th>Notifications</th>
+              <th>First Name</th>
+              <th>Last Name</th>
+              <th>Username</th>
+            </tr>
+          </thead>
+          <tbody>{this.renderPost()}</tbody>
+        </Table>
       </Col>
     );
   }
