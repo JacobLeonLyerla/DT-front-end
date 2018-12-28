c889518c3c4afa7b42f9af345cde94dca05dbbd9
Jacob Leon Lyerla <umasour@yahoo.com>
2018-12-27 18:46:03 -0700

    working on rendering post

--- src/components/post.js
+++ src/components/post.js
@@ -1,10 +1,18 @@
-import React, { Component } from "react";
+import React, { Component,Fragment } from "react";
 import {Col} from "reactstrap"
 class Post extends Component {
+renderPost=()=>{
+          return this.props.post.map(post =>(<Fragment>
+              {console.log(post)}
+          </Fragment>))
+      }
+
   render() {
+      
     return (
         <Col className="table-container" md={`${10 + this.props.tagVar}`}>
-        test
+
+        {this.renderPost()}
       </Col>
     );
   }
