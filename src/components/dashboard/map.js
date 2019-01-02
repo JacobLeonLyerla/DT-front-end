86545e8790b622d992e3e606fcf8dd6676b308c1
Jacob Leon Lyerla <umasour@yahoo.com>
2019-01-01 23:56:51 -0700

    setting up my map so now it takes my props and renders on the map

--- src/components/dashboard/map.js
+++ src/components/dashboard/map.js
@@ -1,57 +1,69 @@
-import React, { Component,Fragment } from "react";
+import React, { Component, Fragment } from "react";
 
 import {
-    withGoogleMap,
-    GoogleMap,
-    Marker,
-    withScriptjs
-  } from "react-google-maps";
-  
+  withGoogleMap,
+  GoogleMap,
+  Marker,
+  withScriptjs
+} from "react-google-maps";
 
-  const MapWithAMarker = withScriptjs(withGoogleMap((props) =>{
+const MapWithAMarker = withGoogleMap(props => {
+  console.log(props);
+  return (
     <GoogleMap
-    
-      defaultZoom={6}
-      defaultCenter={{ lat: -34.397, lng: 150.644 }}
+      defaultZoom={4}
+      center={{
+        lat: props.defaultCenter.lat, lng: props.defaultCenter.lng
+      }}
+      defaultCenter={{
+        lat: props.defaultCenter.lat,
+        lng: props.defaultCenter.lng
+      }}
     >
       <Marker
-        position={{ lat: -34.397, lng: 150.644 }}
+        position={{
+          lat: props.defaultCenter.lat,
+          lng: props.defaultCenter.lng
+        }}
       />
     </GoogleMap>
-  }));
-  class Map extends Component{
-    constructor(props) {
-        super(props);
-      this.state ={
-          defaultCenter:{
-              lat: 0,
-              lng: 0,
+  );
+});
+class Map extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      defaultCenter: {
+        lat: 0,
+        lng: 0
+      }
+    };
+  }
+
+  setupDefault = () => {
+    if (this.props.picture !== undefined) {
+      if (this.state.defaultCenter.lat === 0) {
+        this.setState({
+          defaultCenter: {
+            lat: this.props.picture.lat,
+            lng: this.props.picture.lng
           }
-        }
+        });
       }
-      
-      setupDefault=()=>{
-      if(this.props.picture !== undefined){
-     
-    
-        
-              if(this.state.defaultCenter.lat ===0){
-             this.setState({ defaultCenter:{
-                lat: this.props.picture.lat ,
-                lng: this.props.picture.lng ,
-            }}) 
-          }  }
-      }
-  render(){
-      console.log(this.state)
-      return(<Fragment>
-          {this.setupDefault()}
-  <MapWithAMarker
-    containerElement={<div style={{ height: `400px` }} />}
-    mapElement={<div style={{ height: `100%` }} />}
-    defaultCenter={this.state.defaultCenter}
-  />
-  </Fragment>)
+    }
+  };
+  render() {
+    console.log(this.state);
+    return (
+      <Fragment>
+        {this.setupDefault()}
+        <MapWithAMarker
+          containerElement={<div style={{ height: `45vh` }} />}
+          mapElement={<div style={{ height: `100%` }} />}
+          defaultCenter={this.state.defaultCenter}
+        />
+      </Fragment>
+    );
   }
-  }
-  export default Map;
+}
+export default Map;
