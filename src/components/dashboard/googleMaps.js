9d230145f7c6602077231e9c33cf73ef62f9136a
Jacob Leon Lyerla <umasour@yahoo.com>
2019-01-01 23:12:36 -0700

    passing through more props

--- src/components/dashboard/googleMaps.js
+++ src/components/dashboard/googleMaps.js
@@ -8,7 +8,7 @@ class Maps extends Component  {
   render() {
 
     return (<Fragment>
-      <Map picture={this.props.picture}/> 
+      <Map picture={this.props.picture[0]}/> 
     </Fragment>);
   }
 }
