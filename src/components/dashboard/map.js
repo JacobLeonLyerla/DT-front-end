28b186390c9793f302923c9c983db5cf2c015268
Jacob Leon Lyerla <umasour@yahoo.com>
2019-01-01 22:27:42 -0700

    trying to get my data into state

--- src/components/dashboard/map.js
+++ src/components/dashboard/map.js
@@ -6,8 +6,10 @@ import {
   } from "react-google-maps";
   
   const MapWithAMarker = withGoogleMap(props =>
+   
     <GoogleMap
-      defaultZoom={8}
+    
+      defaultZoom={6}
       defaultCenter={{ lat: -34.397, lng: 150.644 }}
     >
       <Marker
@@ -16,11 +18,35 @@ import {
     </GoogleMap>
   );
   class Map extends Component{
+    constructor(props) {
+        super(props);
+      this.state ={
+          defaultCenter:{
+              lat: 0,
+              lng: 0,
+          }
+        }
+      }
+      componentDidMount(){
+      this.setupDefault()
+      }
+      setupDefault=()=>{
+          console.log(this.props)
+              if(this.props.picture.lat){
+             this.setState({ defaultCenter:{
+                lat: this.props.picture.lat ,
+                lng: this.props.picture.lng ,
+            }}) 
+          }
+      }
   render(){
+      console.log(this.state)
       return(
+          
   <MapWithAMarker
     containerElement={<div style={{ height: `400px` }} />}
     mapElement={<div style={{ height: `100%` }} />}
+    defaultCenter={this.state.defaultCenter}
   />)
   }
   }
