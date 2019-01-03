558e18849d8e236f80007cafeaa178c87e0ebf4c
Jacob Leon Lyerla <umasour@yahoo.com>
2019-01-02 23:14:08 -0700

    passing the data i need added in a ternary to decide what props to send into map

--- src/components/dashboard/googleMaps.js
+++ src/components/dashboard/googleMaps.js
@@ -1,17 +1,26 @@
-import React, { Component,Fragment } from "react";
-import { GoogleMap, Marker } from "react-google-maps"
+import React, { Component, Fragment } from "react";
+import { GoogleMap, Marker } from "react-google-maps";
 import Map from "./map";
 
-
-class Maps extends Component  {
- 
+class Maps extends Component {
   render() {
-
-    return (<Fragment>
-      {this.props.picture ? (<Fragment>
-      <Map picture={this.props.picture[0]} name={this.props.name}/></Fragment>):
-      (<div>hi</div>)}
-    </Fragment>);
+    return (
+      <Fragment>
+        {this.props.picture ? (
+          <Fragment>
+            <Map picture={this.props.picture[0]} name={this.props.name} />
+          </Fragment>
+        ) : (
+          <Fragment>
+            <Map
+              tag={this.props.tag}
+              name={this.props.name}
+              markers={this.props.markers}
+            />
+          </Fragment>
+        )}
+      </Fragment>
+    );
   }
 }
 export default Maps;
