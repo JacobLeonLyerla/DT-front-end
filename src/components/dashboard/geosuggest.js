d5d3a973714ff9eee1227c317a87b2b700859ec5
Jacob Leon Lyerla <umasour@yahoo.com>
2019-01-02 17:26:03 -0700

    setting up the geosuggest it now mostly works now i need to style it

--- src/components/dashboard/geosuggest.js
+++ src/components/dashboard/geosuggest.js
@@ -1,34 +1,26 @@
 /*global google*/
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React,{Component} from 'react';
 import Geosuggest from 'react-geosuggest';
 
-class Geo extends React.Component {
+class Geo extends Component {
+    constructor(props){
+        super(props)
+    }
 
   render() {
-    var fixtures = [
-      {label: 'Old Elbe Tunnel, Hamburg', location: {lat: 53.5459, lng: 9.966576}},
-      {label: 'Reeperbahn, Hamburg', location: {lat: 53.5495629, lng: 9.9625838}},
-      {label: 'Alster, Hamburg', location: {lat: 53.5610398, lng: 10.0259135}}
-    ];
 
     return (
-      <div>
+    
         <Geosuggest
           ref={el=>this._geoSuggest=el}
           placeholder="Start typing!"
-          initialValue="Hamburg"
-          fixtures={fixtures}
-          onSuggestSelect={this.onSuggestSelect}
-          location={new google.maps.LatLng(53.558572, 9.9278215)}
+          onSuggestSelect={(e)=>this.onSuggestSelect(e,this.props)}
+          location={new google.maps.LatLng(34.009055,-118.497106)}
           radius="20" />
 
+
      
-        <button onClick={()=>this._geoSuggest.focus()}>Focus</button>
-        <button onClick={()=>this._geoSuggest.update('New Zealand')}>Update</button>
-        <button onClick={()=>this._geoSuggest.clear()}>Clear</button>
-        <button onClick={()=>this._geoSuggest.selectSuggest()}>Search</button>
-      </div>
+
     )
   }
 
@@ -36,8 +28,12 @@ class Geo extends React.Component {
    * When a suggest got selected
    * @param  {Object} suggest The suggest
    */
-  onSuggestSelect(suggest) {
-    console.log(suggest);
-  }
+  onSuggestSelect(suggest,props) {
+  
+    this._geoSuggest.clear()
+    this._geoSuggest.focus()
+  if(suggest !== undefined){
+      this.props.setLocation(suggest.location);
+  }}
 };
 export default Geo
