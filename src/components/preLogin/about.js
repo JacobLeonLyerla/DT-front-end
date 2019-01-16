c19ace64cd3ebb82f3c761b095ac8eb5ee6566c1
Jacob Leon Lyerla <umasour@yahoo.com>
2019-01-15 20:46:05 -0700

    setting up my about component for viewing my  videos

--- src/components/preLogin/about.js
+++ src/components/preLogin/about.js
@@ -1,13 +1,33 @@
 import React,{Component,Fragment} from 'react';
-import { Player } from 'video-react';
+import YouTube from 'react-youtube';
+import { Card, CardImg, CardText, CardBody,
+  CardTitle, CardSubtitle, Button } from 'reactstrap';
+ 
+
 class About extends Component {
-  render(){
-  return (
-    <Player
-      playsInline
-      poster="/assets/poster.png"
-      src="https://media.w3.org/2010/05/sintel/trailer_hd.mp4"
-    />
-  );}
-};
+  
+  render() {
+    const opts = {
+      height: '100%',
+      width: '100%',
+      playerVars: { // https://developers.google.com/youtube/player_parameters
+        autoplay: 1
+      }
+    };
+ 
+    return (<div className="modal-container">
+      <YouTube
+        videoId="HNFvgneBabE"
+        opts={opts}
+        onReady={this._onReady}
+      />
+      <div className="modal-buttons">
+        <Button>Intro</Button><Button>Viewing</Button><Button>Creating</Button>
+      </div>
+   </div> );
+  }
+ 
+
+}
 export default About
+ 
