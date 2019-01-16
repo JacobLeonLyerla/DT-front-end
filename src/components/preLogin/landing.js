ec8a4e8e4a4795267d80addf9f930656b8401b2a
Jacob Leon Lyerla <umasour@yahoo.com>
2019-01-15 20:46:43 -0700

    added in a modal for rendering my about component

--- src/components/preLogin/landing.js
+++ src/components/preLogin/landing.js
@@ -1,20 +1,40 @@
-import React, { Fragment } from "react";
+import React, {Component, Fragment } from "react";
 import "../../css/index.css";
 import Header from "./header";
 import Nav from "./landingNav";
 import About from "./landingAbout";
-const Landing = () => {
+import Info from "./about"
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
+class Landing extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      modal: false
+    };
+
+    this.toggle = this.toggle.bind(this);
+  }
+  toggle() {
+    this.setState({
+      modal: !this.state.modal
+    });
+  }
+  render(){
   return (
     <Fragment>
       {localStorage.clear()}
       <div className="App mapBackground-styles">
-        <Nav />
+        <Nav toggle={this.toggle}/>
         <br />
+        
         <Header />
       </div>
+      <Modal isOpen={this.state.modal} toggle={this.toggle} className={this.props.className}>
+      <Info/>
+</Modal>
       <About />
     </Fragment>
-  );
+  );}
 };
 
 export default Landing;
