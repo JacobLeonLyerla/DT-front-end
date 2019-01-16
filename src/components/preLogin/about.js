657f4e9209907ff111b463c811c9a288694a7744
Jacob Leon Lyerla <umasour@yahoo.com>
2019-01-15 21:21:03 -0700

    created a modal that renders videos currently i am using place holders but i will add in my own videos for how the site works tomorrow

--- src/components/preLogin/about.js
+++ src/components/preLogin/about.js
@@ -1,33 +1,51 @@
-import React,{Component,Fragment} from 'react';
-import YouTube from 'react-youtube';
-import { Card, CardImg, CardText, CardBody,
-  CardTitle, CardSubtitle, Button } from 'reactstrap';
- 
+import React, { Component, Fragment } from "react";
+import YouTube from "react-youtube";
+import {
+  Card,
+  CardImg,
+  CardText,
+  CardBody,
+  CardTitle,
+  CardSubtitle,
+  Button
+} from "reactstrap";
 
 class About extends Component {
-  
+  state = {
+    vidieo: "HNFvgneBabE"
+  };
+
   render() {
     const opts = {
-      height: '100%',
-      width: '100%',
-      playerVars: { // https://developers.google.com/youtube/player_parameters
+      height: "100%",
+      width: "100%",
+      playerVars: {
+        // https://developers.google.com/youtube/player_parameters
         autoplay: 1
       }
     };
- 
-    return (<div className="modal-container">
-      <YouTube
-        videoId="HNFvgneBabE"
-        opts={opts}
-        onReady={this._onReady}
-      />
-      <div className="modal-buttons">
-        <Button>Intro</Button><Button>Viewing</Button><Button>Creating</Button>
-      </div>
-   </div> );
-  }
- 
 
+    return (
+      <div className="modal-container">
+      <div className="youtube">
+        <YouTube
+          videoId={this.state.vidieo}
+          opts={opts}
+          onReady={this._onReady}
+        /></div>
+        <div className="modal-buttons">
+          <Button onClick={() => this.setState({ vidieo: "HNFvgneBabE" })}>
+            Intro
+          </Button>
+          <Button onClick={() => this.setState({ vidieo: "jFycqnOpifQ" })}>
+            Viewing
+          </Button>
+          <Button onClick={() => this.setState({ vidieo: "B9JjlwtFImU" })}>
+            Creating
+          </Button>
+        </div>
+      </div>
+    );
+  }
 }
-export default About
- 
+export default About;
