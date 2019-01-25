e2d6928ddde37a1527da13a05994fe4826c4a9eb
Jacob Leon Lyerla <umasour@yahoo.com>
2019-01-24 22:34:03 -0700

    commenting the signin component
    adding in more details about how the code works

--- src/components/preLogin/signin.js
+++ src/components/preLogin/signin.js
@@ -8,41 +8,18 @@ class Signup extends Component {
     super(props);
     this.state = {
       username: "",
-      email: "",
-      checkEmail: "",
-      password: "",
-      validate: {
-        emailState: "",
-        dupemail: ""
-      }
+      password: ""
     };
     this.handleChange = this.handleChange.bind(this);
   }
+  // when this component is rendered it clears out local storage
   componentDidMount() {
     localStorage.clear();
   }
-  validateEmail(e) {
-    const emailRex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    const { validate } = this.state;
-    if (emailRex.test(e.target.value)) {
-      validate.emailState = "has-success";
-    } else {
-      validate.emailState = "has-danger";
-    }
-    this.setState({ validate });
-  }
-  emailMatch = e => {
-    const { validate } = this.state;
-    if (e.target.value === this.state.email) {
-      validate.checkEmailState = "has-success";
-    } else {
-      validate.checkEmailState = "has-danger";
-    }
-    this.setState({ validate });
-  };
+
   handleChange = event => {
     const { target } = event;
-    const value = target.type === "checkbox" ? target.checked : target.value;
+    const { value } = target;
     const { name } = target;
     this.setState({
       [name]: value
@@ -50,17 +27,22 @@ class Signup extends Component {
   };
   handleSubmit = e => {
     e.preventDefault();
+    // clear ou storage and reomove id
+    // i was having issues with this.
     localStorage.clear();
     localStorage.removeItem("id");
     const user = {
       username: this.state.username,
       password: this.state.password
     };
+    // make a post to our login route that returns a  token and user id
+    // than we set that one local storage for persisting the data
     axios
       .post("https://dt-back-end.herokuapp.com/auth/login", user)
       .then(resp => {
         localStorage.setItem("token", `Bearer ${resp.data.token}`);
         localStorage.setItem("id", resp.data.user._id);
+        // call the load pictures function and push the user to the dashboard
         this.props.loadPictures();
         this.props.history.push("/dashboard");
       })
@@ -78,22 +60,10 @@ class Signup extends Component {
               name="username"
               id="username"
               value={this.state.username}
-              // placeholder="myemail@email.com"
-              // value={this.state.email}
-              // valid={this.state.validate.emailState === "has-success"}
-              // invalid={this.state.validate.emailState === "has-danger"}
               onChange={e => {
-                // this.validateEmail(e);
                 this.handleChange(e);
               }}
             />
-            <FormFeedback valid>
-              That's a tasty looking email you've got there.
-            </FormFeedback>
-            <FormFeedback invalid>
-              Uh oh! Looks like there is an issue with your email. Please input
-              a correct email.
-            </FormFeedback>{" "}
           </FormGroup>
           <FormGroup>
             <Label for="examplePassword">Please Enter a Password</Label>
@@ -104,7 +74,6 @@ class Signup extends Component {
               value={this.state.password}
               onChange={this.handleChange}
             />
-            <FormFeedback>Oh noes! that name is already taken</FormFeedback>
           </FormGroup>
           <div className="form-buttons">
             <Button type="submit">Sign In</Button>
