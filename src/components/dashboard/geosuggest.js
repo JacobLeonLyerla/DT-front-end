ad128020d1b2a5a7c65c68ffb3d8ba14b75d03b4
Jacob Leon Lyerla <umasour@yahoo.com>
2019-01-29 23:39:01 -0700

    adding in comments for my geosuggest component

--- src/components/dashboard/geosuggest.js
+++ src/components/dashboard/geosuggest.js
@@ -9,12 +9,16 @@ class Geo extends Component {
 
   render() {
     return (
+      // this is closer to the default geosuggest pretty much
+      // I just added in my own lat and lng and wided the radius
+      // I thought about asking to put in thier own location
+      // but I decided against it for now
       <Geosuggest
         ref={el => (this._geoSuggest = el)}
         placeholder={this.props.placeholder}
         onSuggestSelect={e => this.onSuggestSelect(e, this.props)}
         location={new google.maps.LatLng(34.009055, -118.497106)}
-        radius="20"
+        radius="900000000000000"
       />
     );
   }
