b830f6831e897b7d6841e4f3d92490a49557be09
Jacob Leon Lyerla <umasour@yahoo.com>
2019-01-29 23:38:44 -0700

    commenting my google maps component

--- src/components/dashboard/googleMaps.js
+++ src/components/dashboard/googleMaps.js
@@ -5,6 +5,11 @@ class Maps extends Component {
   render() {
     return (
       <Fragment>
+         {/* picture is only passed in inside of of tagsview and pass in the props
+         so since tag view is for a whole wide region the zoom is less
+         and since the view from the trek component is from the users post
+         this will render a closer zoom of 9.
+         also since the tag view has no markers those are not passing in */}
         {this.props.picture ? (
           <Fragment>
             <Map zoom={4} picture={this.props.picture} name={this.props.name} />
