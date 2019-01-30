00ae7c4ab406b922482ee94ccd8dea903a64f2c3
Jacob Leon Lyerla <umasour@yahoo.com>
2019-01-29 23:37:13 -0700

    formatting my tag component

--- src/components/dashboard/tags.js
+++ src/components/dashboard/tags.js
@@ -4,10 +4,10 @@ import Pictures from "./renderPictures.js";
 class Tags extends Component {
   render() {
     return (
-      // this column is part of the row with dashboard 
+      // this column is part of the row with dashboard
       // and will be sized based on the variables passed in on props
       <Col md={`${10 + this.props.tagVar}`} className="tags-container">
-      {/* we already had the pictures, sent in as props.
+        {/* we already had the pictures, sent in as props.
       pictures are just the categories that load in.
       so we pass them into our pictures component and it renders them for us
       neat right!? */}
