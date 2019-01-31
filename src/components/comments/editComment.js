544c18818614943047af057c760188bb5da1a7eb
Jacob Leon Lyerla <umasour@yahoo.com>
2019-01-30 22:28:51 -0700

    added comments to my edit comment component

--- src/components/comments/editComment.js
+++ src/components/comments/editComment.js
@@ -18,26 +18,34 @@ class EditComment extends Component {
       modal: !this.state.modal
     });
   }
-
+  // this is our main function it handles the edit of the comment
   handleEditComment = () => {
+    // first we make an edit object
     let edit = {};
+    // we than set the key of comment to this.state.comment
     edit.comment = this.state.comment;
+    // we use the id that was passed from the edit component to know what comment we want to edit
+    // than we run a put request targeting that compoennt by the id and updating it with the new edit object
     axios
       .put(`https://dt-back-end.herokuapp.com/comments/${this.props.id}`, edit)
       .then(response => {
+        // close the modal
         this.toggle();
+        // call the setup comments function for rending data and set our reply flag to true
         this.props.setupComments(this.props.propsId);
         this.props.replyflag("true");
       })
       .catch(err => {
       });
   };
-
+  // input takes the name and value from our input target and uses that to set the state
   handleInput = input => {
     this.setState({ [input.target.name]: input.target.value });
   };
   renderModal = () => {
     return (
+      // this is our modal we wrapped it in gragment because it's alreadty being rendered inside of edit
+      // inside of a div
       <Fragment>
         <i class="fas fa-edit" onClick={this.toggle} />
         <Modal
