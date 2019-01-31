88acdf57a4e9cf8e27b0c9ac43220a97572508b3
Jacob Leon Lyerla <umasour@yahoo.com>
2019-01-30 21:54:37 -0700

    commented the delete component

--- src/components/comments/delete.js
+++ src/components/comments/delete.js
@@ -17,22 +17,25 @@ class Delete extends Component {
       modal: !this.state.modal
     });
   }
+  // when the user says yes to wanting to delete the comment we call this function
   handleDelete = () => {
-    console.log(this.props.id);
+    // the function uses a delete method pulling the id off of props that was passed in from the comments component
     axios
       .delete(`https://dt-back-end.herokuapp.com/comments/${this.props.id}`)
       .then(response => {
-        console.log(response.data);
+      // close the modal
         this.toggle();
-
+        // call setupcomments
         this.props.setupComments(this.props.propsId);
+        // and set the reply flag to true 
         this.props.replyflag("true");
       })
       .catch(err => {
-        console.log(err);
       });
   };
-
+// when the user wants to delete sometihng they click delete
+// this modal is rendered and it asked them if they are sure
+// if they are than we actually delete the item
   renderModal = () => {
     return (
       <Fragment>
