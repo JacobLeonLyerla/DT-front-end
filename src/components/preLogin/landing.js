24e5b73f5bbe4d6a682c4444986d675a47f96ce8
Jacob Leon Lyerla <umasour@yahoo.com>
2019-01-31 23:07:33 -0700

    landing spaced out to make it more readable

--- src/components/preLogin/landing.js
+++ src/components/preLogin/landing.js
@@ -1,43 +1,76 @@
 import React, { Component, Fragment } from "react";
+
 import "../../css/index.css";
+
 import Header from "./header";
+
 import Nav from "./landingNav";
+
 import About from "./landingAbout";
+
 import Info from "./about";
+
 import { Modal } from "reactstrap";
+
 class Landing extends Component {
+
   constructor(props) {
+
     super(props);
+
     this.state = {
+
       modal: false
+
     };
 
     this.toggle = this.toggle.bind(this);
+
   }
   toggle() {
+
     this.setState({
+
       modal: !this.state.modal
+
     });
+
   }
   render() {
+
     return (
+
       <Fragment>
+
         {localStorage.clear()}
+
         <div className="App mapBackground-styles">
+
           <Nav toggle={this.toggle} />
+
           <br />
 
           <Header />
+
         </div>
+
         <Modal
+
           isOpen={this.state.modal}
+
           toggle={this.toggle}
+
           className="modal-about"
+
         >
           <Info />
+
         </Modal>
+
         <About />
+
       </Fragment>
+      
     );
   }
 }
