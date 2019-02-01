445117202cd80bae136efb24f6ee9cb00ff5bdfe
Jacob Leon Lyerla <umasour@yahoo.com>
2019-01-31 23:02:57 -0700

    geosuggest

--- src/components/dashboard/geosuggest.js
+++ src/components/dashboard/geosuggest.js
@@ -1,5 +1,6 @@
 /*global google*/
 import React, { Component } from "react";
+
 import Geosuggest from "react-geosuggest";
 
 class Geo extends Component {
@@ -8,14 +9,22 @@ class Geo extends Component {
   render() {
     return (
       // this is closer to the default geosuggest pretty much
+
       // I just added in my own lat and lng and wided the radius
+
       // I thought about asking to put in thier own location
+
       // but I decided against it for now
       <Geosuggest
+
         ref={el => (this._geoSuggest = el)}
+
         placeholder={this.props.placeholder}
+
         onSuggestSelect={e => this.onSuggestSelect(e, this.props)}
+
         location={new google.maps.LatLng(34.009055, -118.497106)}
+
         radius="900000000000000"
       />
     );
@@ -24,13 +33,20 @@ class Geo extends Component {
   /**
    * When a suggest got selected
    * @param  {Object} suggest The suggest
+   * 
    */
-  onSuggestSelect(suggest, props) {
+  onSuggestSelect(suggest) {
+
     this._geoSuggest.clear();
+
     this._geoSuggest.focus();
+
     if (this.props.name === "location") {
+
       if (suggest !== undefined) {
+
         this.props.setLocation(suggest.location, suggest.label, "location");
+        
       }
     }
   }
