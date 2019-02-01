97fef09e9a16e25262cf64e0d1542f05e09c5b82
Jacob Leon Lyerla <umasour@yahoo.com>
2019-01-31 23:03:09 -0700

    googlemaps spaced out to make it more readable

--- src/components/dashboard/googleMaps.js
+++ src/components/dashboard/googleMaps.js
@@ -1,30 +1,49 @@
 import React, { Component, Fragment } from "react";
+
 import Map from "./map";
 
 class Maps extends Component {
   render() {
     return (
+
       <Fragment>
          {/* picture is only passed in inside of of tagsview and pass in the props
+
          so since tag view is for a whole wide region the zoom is less
+
          and since the view from the trek component is from the users post
+
          this will render a closer zoom of 9.
+
          also since the tag view has no markers those are not passing in */}
         {this.props.picture ? (
+
           <Fragment>
+
             <Map zoom={4} picture={this.props.picture} name={this.props.name} />
+
           </Fragment>
+
         ) : (
+
           <Fragment>
+
             <Map
               zoom={9}
+              
               tag={this.props.tag}
+
               name={this.props.name}
+
               markers={this.props.markers}
             />
+
           </Fragment>
+
         )}
+
       </Fragment>
+      
     );
   }
 }
