bf8966c7db97ef5d9a4d583a099c63beb03948a9
Jacob Leon Lyerla <umasour@yahoo.com>
2019-01-31 23:03:42 -0700

    tag details  spaced out to make it more readable

--- src/components/dashboard/tagDetails.js
+++ src/components/dashboard/tagDetails.js
@@ -6,14 +6,23 @@ class Details extends Component {
       // very basic function just takes the props passed in and renders the
       // infromation about the user the title and the discription
       <div className="details">
+
         <div className="header">
+
           <div className="username">
+
             Posted by <div className="user">{` ${this.props.tag.user}`}</div>
+
           </div>
+
           <div className="name">{this.props.tag.name}</div>
+
         </div>
+
         <div className="description">{this.props.tag.description}</div>
+
       </div>
+      
     );
   }
 }
