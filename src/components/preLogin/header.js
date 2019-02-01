d3ac5ba00953ccfe4bb61d53838d7aabcf9c5baf
Jacob Leon Lyerla <umasour@yahoo.com>
2019-01-31 23:07:24 -0700

    header spaced out to make it more readable

--- src/components/preLogin/header.js
+++ src/components/preLogin/header.js
@@ -2,9 +2,13 @@ import React from "react";
 import logo from "../../assets/logo.png";
 const Header = () => {
   return (
+
     <div className="logo-wrapper">
+
       <img alt="DreamTrekking" className="logo-styles" src={logo} />
+
     </div>
+    
   );
 };
 export default Header;
