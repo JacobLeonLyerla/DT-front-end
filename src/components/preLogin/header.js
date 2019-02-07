e3f09088cedf402693199dc84cd5fb09ab85b9d7
Jacob Leon Lyerla <umasour@yahoo.com>
2019-02-06 22:58:30 -0700

    header component formatted for better readability

--- src/components/preLogin/header.js
+++ src/components/preLogin/header.js
@@ -2,13 +2,9 @@ import React from "react";
 import logo from "../../assets/logo.png";
 const Header = () => {
   return (
-
     <div className="logo-wrapper">
-
       <img alt="DreamTrekking" className="logo-styles" src={logo} />
-
     </div>
-    
   );
 };
 export default Header;
