106dc23c9922b6515d7ce06be748641c2dd413a5
Jacob Leon Lyerla <umasour@yahoo.com>
2019-02-06 22:58:39 -0700

    landing component formatted for better readability

--- src/components/preLogin/landing.js
+++ src/components/preLogin/landing.js
@@ -13,64 +13,43 @@ import Info from "./about";
 import { Modal } from "reactstrap";
 
 class Landing extends Component {
-
   constructor(props) {
-
     super(props);
 
     this.state = {
-
       modal: false
-
     };
 
     this.toggle = this.toggle.bind(this);
-
   }
   toggle() {
-
     this.setState({
-
       modal: !this.state.modal
-
     });
-
   }
   render() {
-
     return (
-
       <Fragment>
-
         {localStorage.clear()}
 
         <div className="App mapBackground-styles">
-
           <Nav toggle={this.toggle} />
 
           <br />
 
           <Header />
-
         </div>
 
         <Modal
-
           isOpen={this.state.modal}
-
           toggle={this.toggle}
-
           className="modal-about"
-
         >
           <Info />
-
         </Modal>
 
         <About />
-
       </Fragment>
-      
     );
   }
 }
