21d0ed7fd20059b654cdc81456d0c746c368ba89
Jacob Leon Lyerla <umasour@yahoo.com>
2019-02-06 22:59:03 -0700

    sign in component formatted for better readability

--- src/components/preLogin/signin.js
+++ src/components/preLogin/signin.js
@@ -7,16 +7,13 @@ import { Link } from "react-router-dom";
 import axios from "axios";
 
 class Signup extends Component {
-
   constructor(props) {
     super(props);
 
     this.state = {
-
       username: "",
 
       password: ""
-
     };
 
     this.handleChange = this.handleChange.bind(this);
@@ -24,13 +21,10 @@ class Signup extends Component {
 
   // when this component is rendered it clears out local storage
   componentDidMount() {
-
     localStorage.clear();
-
   }
 
   handleChange = event => {
-
     const { target } = event;
 
     const { value } = target;
@@ -38,14 +32,11 @@ class Signup extends Component {
     const { name } = target;
 
     this.setState({
-
       [name]: value
-
     });
   };
 
   handleSubmit = e => {
-
     e.preventDefault();
     // clear ou storage and reomove id
     // i was having issues with this.
@@ -55,11 +46,9 @@ class Signup extends Component {
     localStorage.removeItem("id");
 
     const user = {
-
       username: this.state.username,
 
       password: this.state.password
-
     };
     // make a post to our login route that returns a  token and user id
     // than we set that one local storage for persisting the data
@@ -67,7 +56,6 @@ class Signup extends Component {
       .post("https://dt-back-end.herokuapp.com/auth/login", user)
 
       .then(resp => {
-
         localStorage.setItem("token", `Bearer ${resp.data.token}`);
 
         localStorage.setItem("id", resp.data.user._id);
@@ -76,74 +64,50 @@ class Signup extends Component {
         this.props.loadPictures();
 
         this.props.history.push("/dashboard");
-
       })
 
       .catch(() => {});
   };
 
   render() {
-
     return (
-
       <div className="signup-container">
-
         <form onSubmit={this.handleSubmit} className="signup-form">
-
           <FormGroup>
-
             <Label for="exampleEmail">Please Enter username</Label>
 
             <Input
-
               type="username"
-              
               name="username"
-
               id="username"
-
               value={this.state.username}
-
               onChange={e => {
-
                 this.handleChange(e);
               }}
             />
           </FormGroup>
 
           <FormGroup>
-
             <Label for="examplePassword">Please Enter a Password</Label>
 
             <Input
-
               type="password"
-
               name="password"
-
               id="password"
-
               value={this.state.password}
-
               onChange={this.handleChange}
-
             />
           </FormGroup>
           <div className="form-buttons">
             <Button type="submit">Sign In</Button>
-
           </div>
 
           <div className="form-footer">
-
             {`Don't have an account yet? `}
 
             {<Link to="/signup">Sign Up now!</Link>}
-
           </div>
-
         </form>
-        
       </div>
     );
   }
