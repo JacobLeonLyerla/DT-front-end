292c1dd088f6649dd13c25e102b626d436ece56d
Jacob Leon Lyerla <umasour@yahoo.com>
2019-04-04 23:24:49 -0700

    imported my layout function from my helper that renders my jsx

--- src/components/preLogin/signin.js
+++ src/components/preLogin/signin.js
@@ -1,11 +1,6 @@
-import React, { Component } from "react";
+import React, { Component,Fragment } from "react";
 
-import { FormGroup, Label, Input, Button } from "reactstrap";
-
-import { Link } from "react-router-dom";
-
-
-import {handleSubmit} from "../../helpers/signinHelpers"
+import {handleSubmit,signinLayout} from "../../helpers/signinHelpers"
 import {handleChange} from "../../helpers/commonHelpers"
 
 class Signup extends Component {
@@ -32,46 +27,9 @@ class Signup extends Component {
   render() {
     this.handleChange = handleChange.bind(this)
     this.handleSubmit = handleSubmit.bind(this)
-    return (
-      <div className="signup-container">
-        <form onSubmit={this.handleSubmit} className="signup-form">
-          <FormGroup>
-            <Label for="exampleEmail">Please Enter username</Label>
-
-            <Input
-              type="username"
-              name="username"
-              id="username"
-              value={this.state.username}
-              onChange={e => {
-                this.handleChange(e);
-              }}
-            />
-          </FormGroup>
-
-          <FormGroup>
-            <Label for="examplePassword">Please Enter a Password</Label>
-
-            <Input
-              type="password"
-              name="password"
-              id="password"
-              value={this.state.password}
-              onChange={this.handleChange}
-            />
-          </FormGroup>
-          <div className="form-buttons">
-            <Button type="submit">Sign In</Button>
-          </div>
-
-          <div className="form-footer">
-            {`Don't have an account yet? `}
-
-            {<Link to="/signup">Sign Up now!</Link>}
-          </div>
-        </form>
-      </div>
-    );
+    this.signinLayout = signinLayout.bind(this)
+    return <Fragment>{this.signinLayout()}</Fragment>
+   
   }
 }
 export default Signup;
