30e86c24f7bb6098c79895cacab5a6fc5ee51d46
Jacob Leon Lyerla <umasour@yahoo.com>
2019-04-04 23:20:01 -0700

    moved handle submit to my helper file

--- src/components/preLogin/signin.js
+++ src/components/preLogin/signin.js
@@ -4,8 +4,8 @@ import { FormGroup, Label, Input, Button } from "reactstrap";
 
 import { Link } from "react-router-dom";
 
-import axios from "axios";
 
+import {handleSubmit} from "../../helpers/signinHelpers"
 import {handleChange} from "../../helpers/commonHelpers"
 
 class Signup extends Component {
@@ -28,41 +28,10 @@ class Signup extends Component {
 
   
 
-  handleSubmit = e => {
-    e.preventDefault();
-    // clear ou storage and reomove id
-    // i was having issues with this.
-
-    localStorage.clear();
-
-    localStorage.removeItem("id");
-
-    const user = {
-      username: this.state.username,
-
-      password: this.state.password
-    };
-    // make a post to our login route that returns a  token and user id
-    // than we set that one local storage for persisting the data
-    axios
-      .post("https://dt-back-end.herokuapp.com/auth/login", user)
-
-      .then(resp => {
-        localStorage.setItem("token", `Bearer ${resp.data.token}`);
-
-        localStorage.setItem("id", resp.data.user._id);
-
-        // call the load pictures function and push the user to the dashboard
-        this.props.loadPictures();
-
-        this.props.history.push("/dashboard");
-      })
-
-      .catch(() => {});
-  };
-
+ 
   render() {
     this.handleChange = handleChange.bind(this)
+    this.handleSubmit = handleSubmit.bind(this)
     return (
       <div className="signup-container">
         <form onSubmit={this.handleSubmit} className="signup-form">
