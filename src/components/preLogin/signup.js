d61bdc0539ad90df93bcdce12aa3fdeef1e31202
Jacob Leon Lyerla <umasour@yahoo.com>
2019-04-04 23:39:45 -0700

    removed validate email moved it to helper

--- src/components/preLogin/signup.js
+++ src/components/preLogin/signup.js
@@ -14,6 +14,7 @@ import { Link } from "react-router-dom";
 import axios from "axios";
 
 import { handleChange } from "../../helpers/commonHelpers";
+import { validateEmail } from "../../helpers/signupHelpers";
 
 class Signup extends Component {
   constructor(props) {
@@ -44,25 +45,8 @@ class Signup extends Component {
         password2State: ""
       }
     };
-
   }
-  // this function runs basic regualar expression to check that the email is formatted correctly
-  validateEmail(e) {
-    const emailRex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
-    // deconstruct validate from the object we set on state
-    const { validate } = this.state;
-
-    // we test the value we passed in  and if it passed return success or failure
-    if (emailRex.test(e.target.value)) {
-      validate.emailState = "has-success";
-    } else {
-      validate.emailState = "has-danger";
-    }
-
-    // set that object back on state
-    this.setState({ validate });
-  }
   // this function uses regualar expression to check password strength
   passwordStrength(e) {
     // I found this regex line is very basic and just checks for length that it has a digit in it
@@ -153,6 +137,7 @@ class Signup extends Component {
 
   render() {
     this.handleChange = handleChange.bind(this);
+    this.validateEmail = validateEmail.bind(this);
     return (
       <div className="signup-container">
         {/* set up a form with an on submit for when the user hits enter or pressed the button */}
