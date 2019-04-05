1da86e2b37fa5eca6745f05b2a847a0679bf584b
Jacob Leon Lyerla <umasour@yahoo.com>
2019-04-04 23:43:35 -0700

    created email match function helper

--- src/helpers/signupHelpers.js
+++ src/helpers/signupHelpers.js
@@ -1,35 +1,48 @@
 // this function runs basic regualar expression to check that the email is formatted correctly
 export function validateEmail(e) {
-    const emailRex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  const emailRex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
-    // deconstruct validate from the object we set on state
-    const { validate } = this.state;
+  // deconstruct validate from the object we set on state
+  const { validate } = this.state;
 
-    // we test the value we passed in  and if it passed return success or failure
-    if (emailRex.test(e.target.value)) {
-      validate.emailState = "has-success";
-    } else {
-      validate.emailState = "has-danger";
-    }
-
-    // set that object back on state
-    this.setState({ validate });
+  // we test the value we passed in  and if it passed return success or failure
+  if (emailRex.test(e.target.value)) {
+    validate.emailState = "has-success";
+  } else {
+    validate.emailState = "has-danger";
   }
 
-   // this function uses regualar expression to check password strength
-   export function passwordStrength(e) {
-    // I found this regex line is very basic and just checks for length that it has a digit in it
-    const mediumRegex = /^(((?=.*[a-z])(?=.*[A-Z]))|((?=.*[a-z])(?=.*[0-9]))|((?=.*[A-Z])(?=.*[0-9])))(?=.{6,})/;
+  // set that object back on state
+  this.setState({ validate });
+}
 
-    // deconstruct the validate from state
-    const { validate } = this.state;
+// this function uses regualar expression to check password strength
+export function passwordStrength(e) {
+  // I found this regex line is very basic and just checks for length that it has a digit in it
+  const mediumRegex = /^(((?=.*[a-z])(?=.*[A-Z]))|((?=.*[a-z])(?=.*[0-9]))|((?=.*[A-Z])(?=.*[0-9])))(?=.{6,})/;
 
-    // we test that if the value passes the regex
-    if (mediumRegex.test(e.target.value)) {
-      validate.passwordState = "has-success";
-    } else {
-      validate.passwordState = "has-danger";
-    }
-    // we set that new value onto state
-    this.setState({ validate });
+  // deconstruct the validate from state
+  const { validate } = this.state;
+
+  // we test that if the value passes the regex
+  if (mediumRegex.test(e.target.value)) {
+    validate.passwordState = "has-success";
+  } else {
+    validate.passwordState = "has-danger";
   }
+  // we set that new value onto state
+  this.setState({ validate });
+}
+
+// this checks the first the email from the value passed in matches the email on state
+// the value is the second email input
+export function emailMatch(e) {
+  const { validate } = this.state;
+
+  if (e.target.value === this.state.email) {
+    validate.checkEmailState = "has-success";
+  } else {
+    validate.checkEmailState = "has-danger";
+  }
+  this.setState({ validate });
+}
