913717eadeba33a6ce463cbd5e2759a1d0234fea
Jacob Leon Lyerla <umasour@yahoo.com>
2021-10-27 13:01:22 -0700

    swapped to npm from yarn and removed our about modal

--- src/components/preLogin/landingNav.js
+++ src/components/preLogin/landingNav.js
@@ -7,15 +7,7 @@ class LandingNav extends Component {
     return (
       <div className="landing-Nav">
         <Nav>
-          <NavItem>
-            <NavLink
-              onClick={() => this.props.toggle()}
-              style={{ textAlign: "left", color: "white", cursor: "pointer" }}
-              className="about"
-            >
-              About
-            </NavLink>
-          </NavItem>
+        
 
           <NavItem>
             <Link
