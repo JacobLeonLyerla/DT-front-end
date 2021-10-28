406c360e85be398b59b6c360cffe1a639b171d8d
Jacob Leon Lyerla <umasour@yahoo.com>
2021-10-28 03:36:23 -0700

    made a picture card component

---
+++ src/components/pictureCard/pictureCard.component.jsx
@@ -0,0 +1,57 @@
+import React from 'react'
+import IMAGES from "../../assets/images.js";
+import {
+    Col,
+    Row,
+    CardImg,
+    Card,
+    CardImgOverlay,
+    CardText,
+  } from "reactstrap";
+  
+  import { Link } from "react-router-dom";
+const PictureCard = (props) => {
+    const {img} = props
+    console.log(img)
+    const imgString = img.replace(/-/g, "").toLowerCase();
+    console.log(imgString)
+    return (
+        // we use a column with an inline style for the background
+        // we also use our img function to import the imgages by their name
+        //hoever we have to remove any dashes that were on the names in the database
+        
+       
+        <Col
+          data-ca="ca"
+          className={`tag-img tags`}
+          md="6"
+        >
+          <Card  className={`tag-img tags`}>
+            <Link
+              to={`/dashboard/${img.toLowerCase()}`}
+              style={{ textDecoration: "none" }}
+            >
+              <div className="cover ">
+
+                <CardImg
+                  className={`tag-img tags`}
+                  alt="Card image cap"
+                  src={imgString.split(".").reduce((o, i) => o[i], IMAGES)}
+                  top
+                  width="100%"
+                />
+                <CardImgOverlay>
+                  <CardText >
+                  {img.replace(/-/g, " ")}
+                  </CardText>
+                </CardImgOverlay>
+              </div>
+            </Link>
+          </Card>
+        </Col>
+
+      );
+    
+}
+
+export default PictureCard
