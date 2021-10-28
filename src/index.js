fd2de5a290d6190a5579d26973a58316b0a1c20e
Jacob Leon Lyerla <umasour@yahoo.com>
2021-10-28 03:35:29 -0700

    context

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
+import ContextProvider from "./context/ContextProvider";
+
 import "./index.css";
 import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
 ReactDOM.render(
+  <ContextProvider>
   <Router baseurl="/">
+    
     <App />
-  </Router>,
+
+  </Router>
+  </ContextProvider>
+,
   document.getElementById("root")
 );
