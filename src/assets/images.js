9f035fa7b59bdcfe29a70566d6eeb6800d6ec93e
Jacob Leon Lyerla <umasour@yahoo.com>
2021-10-27 19:36:23 -0700

    file for exporting images

---
+++ src/assets/images.js
@@ -0,0 +1,7 @@
+const IMAGES =  {
+    asia: require("./asia.jpg").default,
+    america: require("./america.jpeg").default,
+    china: require("./china.jpeg").default,
+    africa: require("./africa.jpg").default
+}
+export default IMAGES
