8f24a2156dfa93a1ff9990b8db327c0ab1cdd24c
Jacob Leon Lyerla <umasour@yahoo.com>
2021-10-27 19:37:17 -0700

    I created dummy data to render our components

--- src/components/dashboard/tags.js
+++ src/components/dashboard/tags.js
@@ -5,7 +5,62 @@ import { Col } from "reactstrap";
 import Pictures from "./renderPictures.js";
 
 class Tags extends Component {
+
+
+
   render() {
+
+    const picture = [
+      { name: "Asia",
+     tag: [],
+     country: "Asia",
+   
+     region: "Asia",
+   
+     city: "String",
+   
+     priority: 1,
+   
+     createdOn:new Date(),
+   },
+   { name: "America",
+   tag: [],
+   country: "Asia",
+ 
+   region: "Asia",
+ 
+   city: "String",
+ 
+   priority: 1,
+ 
+   createdOn:new Date(),
+ },
+ { name: "China",
+ tag: [],
+ country: "China",
+
+ region: "Asia",
+
+ city: "String",
+
+ priority: 3,
+
+ createdOn:new Date(),
+},
+{ name: "Africa",
+tag: [],
+country: "Africa",
+
+region: "Africa",
+
+city: "Africa",
+
+priority: 2,
+
+createdOn:new Date(),
+},
+   
+   ]
     return (
       // this column is part of the row with dashboard
       // and will be sized based on the variables passed in on props
@@ -14,7 +69,7 @@ class Tags extends Component {
       pictures are just the categories that load in.
       so we pass them into our pictures component and it renders them for us
       neat right!? */}
-        <Pictures pictures={this.props.pictures} />
+        <Pictures pictures={picture} />
       </Col>
     );
   }
