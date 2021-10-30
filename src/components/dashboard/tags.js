886cc78326257de70811db7617574a84b21a2312
Jacob Leon Lyerla <umasour@yahoo.com>
2021-10-29 23:17:03 -0700

    removed dummy data

--- src/components/dashboard/tags.js
+++ src/components/dashboard/tags.js
@@ -1,77 +1,27 @@
-import React, { Component } from "react";
+import React, { useContext,useEffect } from "react";
 
 import { Col } from "reactstrap";
 
 import Pictures from "./renderPictures.js";
-
-class Tags extends Component {
-
-
-
-  render() {
-
-    const picture = [
-      { name: "Asia",
-     tag: [],
-     country: "Asia",
-   
-     region: "Asia",
-   
-     city: "String",
-   
-     priority: 1,
-   
-     createdOn:new Date(),
-   },
-   { name: "America",
-   tag: [],
-   country: "Asia",
- 
-   region: "Asia",
- 
-   city: "String",
- 
-   priority: 1,
- 
-   createdOn:new Date(),
- },
- { name: "China",
- tag: [],
- country: "China",
-
- region: "Asia",
-
- city: "String",
-
- priority: 3,
-
- createdOn:new Date(),
-},
-{ name: "Africa",
-tag: [],
-country: "Africa",
-
-region: "Africa",
-
-city: "Africa",
-
-priority: 2,
-
-createdOn:new Date(),
-},
-   
-   ]
-    return (
+ import AppContext from "../../context/index.js";
+const Tags= (props)=> {
+  const {example,getPictures} = useContext(AppContext);
+    let pictures = []
+  useEffect(() => {
+      pictures = getPictures()
+  }, [])
+  
+  
+  return(
       // this column is part of the row with dashboard
       // and will be sized based on the variables passed in on props
-      <Col md={`${10 + this.props.tagVar}`} className="tags-container">
+      <Col md={`${10 + props.tagVar}`} className="tags-container">
         {/* we already had the pictures, sent in as props.
       pictures are just the categories that load in.
       so we pass them into our pictures component and it renders them for us
       neat right!? */}
-        <Pictures pictures={picture} />
+        <Pictures pictures={example} />
       </Col>
-    );
-  }
-}
+    );}
+
 export default Tags;
