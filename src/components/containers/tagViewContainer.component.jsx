936645cdb76526ccbd5bb4d3ec4540d0c033a7ef
Jacob Leon Lyerla <umasour@yahoo.com>
2021-10-29 23:07:47 -0700

    created an app to render our components for when we click on a tag

--- src/components/containers/tagViewContainer.component.jsx
+++ src/components/containers/tagViewContainer.component.jsx
@@ -1,23 +1,36 @@
-import React, { useContext } from "react";
+import React, { useContext,useEffect } from "react";
 import RenderTags from "../tagView/renderTags.component";
 import Maps from "../dashboard/googleMaps";
+
 import { Col, Row } from "reactstrap";
 import PictureCard from "../pictureCard/pictureCard.component";
 import AppContext from "../../context";
-const TagViewContainer = () => {
-  const { example } = useContext(AppContext);
-  let name = example[0].name;
-  name = name.replace(/-/g, "").toLowerCase();
-  const { tag } = example[0];
+import { withRouter } from "react-router";
+import TableContainer from "../tableContainer/tabelContainer.component";
+const TagViewContainer = ({match}) => {
+
+  const { example,tags,getTags,currentPicture,getCurrentPicture } = useContext(AppContext);
+  
+  const setPicture = currentPicture[0]
+  const { tag } = setPicture ?setPicture:example[0]
+  useEffect(() => {
+   
+    const {id}=match.params
+     console.log(id)
+    getTags(id)
+    getCurrentPicture(id)
+}, [])
   return (
-    <Col className="tags-container col-md-10">
-      <Row>
+    <Col className="table-container col-md-10">
+       {tags.length!==0?<TableContainer colOneLabel="Title" colTwoLabel="Location" colThreeLabel="Tags" />:null}
+      <Row className="tag-filtered">
+     
         {tag.map((img) => {
-          return <PictureCard img={img} className="tag-filtered" />;
+          return <PictureCard img={img} pictureStyle="tag-filtered" />;
         })}
       </Row>
     </Col>
   );
 };
 
-export default TagViewContainer;
+export default withRouter(TagViewContainer);
