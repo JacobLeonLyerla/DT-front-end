a041bb42617041aa65eb666c5823c00f98f99d85
Jacob Leon Lyerla <umasour@yahoo.com>
2021-11-03 23:48:13 -0700

    tag comments is rendering our comments and replies
    replies are not rerendering correctly trying to figure it out

--- src/components/comments/tagComments.compnent.jsx
+++ src/components/comments/tagComments.compnent.jsx
@@ -2,24 +2,67 @@ import React, { useEffect, useContext } from "react";
 import AppContext from "../../context";
 import { withRouter } from "react-router";
 
-import Reply from "../reply/reply.component"
+import { Button, Input, Form } from "reactstrap";
+import Reply from "./reply/reply.component"
+import Replies from "./replies/replies.component";
 const TagComments = ({ match }) => {
-  const { user,comments, getComments,loadUser } = useContext(AppContext);
+  const {
+    comments,
+    getComments,
+    loadUser,
+    user,
+    comment,
+    setComment,
+    handleComment,
+    getTagById,
+  } = useContext(AppContext);
   useEffect(() => {
     const { id } = match.params;
-    loadUser()
-    getComments(id)
+    loadUser();
+    getComments(id);
+    getTagById(id);
   }, []);
-  return <div className="comments-container">{comments.map(comment =>(
-    
-          <div className="comment-container">
+  return (
+    <>
+      {" "}
+      <div className="comments-container">  {comments.map((comment) => (
+      
+          <div key={comment._id} className="comment-container">
+            {comment.username !== user.username ? (
+              <>
+                <Reply comment={comment} />
+                <div className="username">{comment.username}</div>
+              </>
+            ) : (
+              <>
+                Edit
+                <div className="username">You Commented</div>
+              </>
+            )}
 
-          <Reply comment={comment}/>
-
-
+            <div className="comment">{comment.comment}</div>
+            <div className="replies"><Replies comment={comment}/></div>
           </div>
-     
-  ))}</div>;
+        
+      ))}</div>
+      <Form className="comment-form">
+        <Button
+          className="btn-post"
+          onClick={() => handleComment()}
+          color="primary"
+        >
+          Post
+        </Button>
+        <Input
+          name="comment"
+          value={comment}
+          onChange={(e) => setComment(e.target.value)}
+          type="textarea"
+          style={{ height: "10vh" }}
+        />
+      </Form>
+    </>
+  );
 };
 
 export default withRouter(TagComments);
