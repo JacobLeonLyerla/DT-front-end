3db6864736dee359b18d494bfd1954292a823919
Jacob Leon Lyerla <umasour@yahoo.com>
2021-11-03 23:50:10 -0700

    post form for our post data

--- src/components/post/postForm.component.jsx
+++ src/components/post/postForm.component.jsx
@@ -1,102 +1,141 @@
-import React,{useContext} from 'react'
-import AppContext from '../../context';
-import Geosuggest from '../dashboard/geosuggest';
-import setLocation from './postHelpers/setLocation';
+import React, { useContext,useEffect } from "react";
+import AppContext from "../../context";
+import Geosuggest from "../dashboard/geosuggest";
 import {
-    Button,
-    Col,
-    Row,
-    Badge,
-    Form,
-    Input,
-    DropdownItem,
-    UncontrolledDropdown,
-    DropdownToggle,
-    DropdownMenu,
-    Modal,
-    ModalHeader,
-    ModalBody,
-    ModalFooter
-  } from "reactstrap";
-  import Catagories from './postHelpers/catagories.component';
-  import PickedTags from './postHelpers/pickedTags.component';
-  import Locations from './postHelpers/locations.component';
+  Button,
+
+  Form,
+  Input,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  Modal,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+} from "reactstrap";
+import Catagories from "./postHelpers/catagories.component";
+import PickedTags from "./postHelpers/pickedTags.component";
+import Locations from "./postHelpers/locations.component";
 const PostForm = () => {
-    const { pictures,setCurrentToggleState,toggle,title,description,setTitle,setDescription,filteredLocationsArr, filteredTagsArr,setToggle } = useContext(AppContext);
+  const {
+    getPictures,
+    pictures,
+    setCurrentToggleState,
+    toggle,
+    title,
+    description,
+    setTitle,
+    setDescription,
+    filteredLocationsArr,
+    filteredTagsArr,
+    setToggle,
+    setFilteredLocationsArr,
+  } = useContext(AppContext);
+useEffect(() => {
+  getPictures()
 
-    return (
-        
-            <>
-              {/* this is our dropdown we call render categories to fill out the dropdown  */}
-              <Form className="create-form">
-                <UncontrolledDropdown direction="left">
-                  <DropdownToggle  color="primary" caret>
-                    Tags
-                  </DropdownToggle>
-      
-                  <DropdownMenu><Catagories pictures={pictures}/></DropdownMenu>
-                </UncontrolledDropdown>
-      
-                {/* title input is here this is where the name input is rendered  */}
-                <div className="text-left">Title</div>
-      
-                <Input
-                  name="title"
-                  value={title}
-                  onChange={(e)=>setTitle(e.target.value)}
-                />
-      
-                {/* this is where our badges for the tags is rendered right under the title input bar */}
-                <div className="tag-badge">{filteredTagsArr.length>0?<PickedTags/>:null}</div>
-      
-                {/* description input is rendered here we use this for the user to type in thier description */}
-                <div className="text-left">Description</div>
-      
-                <Input
-                  type="textarea"
-                  style={{ height: "30vh" }}
-                  name="description"
-                  value={description}
-                  onChange={(e)=>setDescription(e.target.value)}
-                />
-                {/* the location is rendered right below the description input box */}
-                {filteredLocationsArr.length>0?<Locations/>:null}
-      
-                <br />
-      
-                <br />
-                {/* this button is to render our modal that allows us to set up the user location  */}
-                <Button className="btn-location" onClick={setToggle}>
-                  Add Location
-                </Button>
-      
-                <Modal
-                  isOpen={toggle}
-                  toggle={setCurrentToggleState}
-                  className={` geo-modal`}
-                >
-                  <ModalHeader toggle={setToggle}>select location</ModalHeader>
-      
-                  <ModalBody>
-                    <div className="modal-badges">{filteredLocationsArr.length>0?<Locations/>:null}</div>
-      
-                    <Geosuggest
-                      setLocation={setLocation}
-                      name={"location"}
-                      placeholder={"Add Location"}
-                    />
-                  </ModalBody>
-      
-                  <ModalFooter>
-                    <Button color="secondary" onClick={setToggle}>
-                      Close
-                    </Button>
-                  </ModalFooter>
-                </Modal>
-              </Form>
-            </>
-          );
+}, [])
+  
+  const setLocation = (cords, name) => {
     
-}
 
-export default PostForm
+    // when the type is location it will create an object passing in the cords
+    // the cords is lat and lng and than it also adds on the name
+
+      let location = filteredLocationsArr;
+
+      let placeObj = {};
+
+      placeObj.name = name;
+
+      placeObj.cords = cords;
+
+      location.push(placeObj);
+      console.log(location)
+      setFilteredLocationsArr(location)
+      setToggle()
+      return 
+ 
+    }
+   
+  return (
+    <>
+      {/* this is our dropdown we call render categories to fill out the dropdown  */}
+      <Form className="create-form">
+        <UncontrolledDropdown direction="left">
+          <DropdownToggle color="primary" caret>
+            Tags
+          </DropdownToggle>
+
+          <DropdownMenu>
+            <Catagories pictures={pictures} />
+          </DropdownMenu>
+        </UncontrolledDropdown>
+
+        {/* title input is here this is where the name input is rendered  */}
+        <div className="text-left">Title</div>
+
+        <Input
+          name="title"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+
+        {/* this is where our badges for the tags is rendered right under the title input bar */}
+        <div className="tag-badge">
+          {filteredTagsArr.length > 0 ? <PickedTags /> : null}
+        </div>
+
+        {/* description input is rendered here we use this for the user to type in thier description */}
+        <div className="text-left">Description</div>
+
+        <Input
+          type="textarea"
+          style={{ height: "30vh" }}
+          name="description"
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+        />
+        {/* the location is rendered right below the description input box */}
+        {filteredLocationsArr.length > 0 ? <Locations /> : null}
+
+        <br />
+
+        <br />
+        {/* this button is to render our modal that allows us to set up the user location  */}
+        <Button className="btn-location" onClick={setToggle}>
+          Add Location
+        </Button>
+
+        <Modal
+          isOpen={toggle}
+          toggle={setCurrentToggleState}
+          className={` geo-modal`}
+        >
+          <ModalHeader toggle={setToggle}>select location</ModalHeader>
+
+          <ModalBody>
+            <div className="modal-badges">
+              {filteredLocationsArr.length > 0 ? <Locations /> : null}
+            </div>
+
+            <Geosuggest
+              setLocation={ setLocation}
+              name={"location"}
+              placeholder={"Add Location"}
+            />
+          </ModalBody>
+
+          <ModalFooter>
+            <Button color="secondary" onClick={setToggle}>
+              Close
+            </Button>
+          </ModalFooter>
+        </Modal>
+      </Form>
+    </>
+  );
+};
+
+export default PostForm;
