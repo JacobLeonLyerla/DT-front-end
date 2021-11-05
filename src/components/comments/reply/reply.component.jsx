da41add318b9dfb6ea3a3b3a8f7346178a4dd286
Jacob Leon Lyerla <umasour@yahoo.com>
2021-11-04 17:46:02 -0700

    set up reply component and allowed it to re render our comments when it is done

--- src/components/comments/reply/reply.component.jsx
+++ src/components/comments/reply/reply.component.jsx
@@ -1,53 +1,55 @@
-import React,{useContext} from 'react'
+import React,{useContext,useState,useEffect} from 'react'
 import AppContext from '../../../context';
 import {
     Button,
-    Tooltip,
-    Modal,
-    ModalHeader,
-    ModalFooter,
     Input,
     Form
   } from "reactstrap";
 const Reply = ({comment}) => {
-  const { currentTag, reply,setReply,handleReply,setCurrentToggleState,toggle } = useContext(AppContext);
+  const { currentTag, reply,setReply,handleReply, } = useContext(AppContext);
+  const [currentComment,setCurrentComment] = useState({})
+  const [inputToggle,setInputToggle] = useState(true)
+  
+  useEffect(() => {
+      setCurrentComment(comment)
 
-  const renderModal =()=>{
-    return(<>
-      <i
-        onClick={()=>setCurrentToggleState()}
-        id={`reply${currentTag._id}`}
-        class="fas fa-reply"
-      />
-      <Modal
-      isOpen={toggle}
-      toggle={setCurrentToggleState}
-    >
-      <ModalHeader  toggle={()=>setCurrentToggleState()}>{`Reply to ${
-        currentTag.user
-      }`}</ModalHeader>
-
-      <Form>
-        <Input
-          style={{ height: "30vh" }}
-          name="reply"
-          value={reply}
-          onChange={(e)=>setReply(e.target.value)}
-          type="textarea"
-        />
-      </Form>
-      <ModalFooter>
-        <Button color="primary" onClick={() =>handleReply(comment)}>
-          Reply
-        </Button>{" "}
-      </ModalFooter>
-    </Modal></>)
+  
+  }, [])
+  const toggleAndReply =(comment)=>{
+    setInputToggle(!inputToggle)
+    handleReply(comment)
+    
   }
- 
     return (
 <div className="target-comment">
 
-        {renderModal()}
+<>
+{inputToggle?      <i
+        onClick={()=>setInputToggle()}
+        id={`reply${currentTag._id}`}
+        class="fas fa-reply"
+      />
+
+:
+
+    ( <> <Form>
+        <Input
+          
+          name="reply"
+          value={reply}
+          onChange={(e)=>setReply(e.target.value)}
+          className="reply-input"
+          type="textarea"
+        />
+      </Form>
+     
+      {reply!==""?  <Button className="reply-button" color="primary" onClick={() =>toggleAndReply(comment)}>
+         Reply
+        </Button>: <Button className="reply-button" color="primary" onClick={() =>setInputToggle(!inputToggle)}>
+         Close
+        </Button>}</>)}
+     
+  </>
       </div>
     )
 }
