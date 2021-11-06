3b2d1fa04a37477307c0fef2bbeb14debfa9364f
Jacob Leon Lyerla <umasour@yahoo.com>
2021-11-05 23:33:52 -0700

    rendering our map here

--- src/components/containers/tagViewContainer.component.jsx
+++ src/components/containers/tagViewContainer.component.jsx
@@ -1,40 +1,42 @@
 import React, { useContext,useEffect } from "react";
 import RenderTags from "../tagView/renderTags.component";
-import Map from "../dashboard/map";
+import Map from "../googleMaps/map.component"
 import { Col, Row } from "reactstrap";
 import PictureCard from "../pictureCard/pictureCard.component";
 import AppContext from "../../context";
 import { withRouter } from "react-router";
 import TableContainer from "./tabelContainer.component";
 import Post from "../post/post.component"
+import HeaderPicture from "../headerPicture/headerPicture.component";
 const TagViewContainer = ({match}) => {
 const {id}=match.params
   const {tags,getTags,currentPicture,getCurrentPicture,loadUser } = useContext(AppContext);
   
   
-  const setPicture =  currentPicture
+  let setPicture =  currentPicture
 
-  const { tag } =  setPicture ?setPicture:{tag:["placeholder","placeholder",]}
+  const { tag } = setPicture ?setPicture:{tag:["placeholder","placeholder",]}
 
   useEffect(() => {
     getTags(id)
     getCurrentPicture(id)
     loadUser()
 }, [])
-
- 
-
-  return (
+if(!currentPicture){
+  setPicture= {}
+}
+  return (<>{currentPicture?
     <Col className="table-container col-md-10">
+     {setPicture.hasOwnProperty("lat")?<Map current={currentPicture} type="picture"/>:<HeaderPicture name={setPicture.name}/>}
       <Post/>
        {tags.length!==0?<TableContainer colOneLabel="Title" colTwoLabel="Location" colThreeLabel="Tags" />:null}
       <Row className="tag-filtered">
        
-        {tag.map((img) => {
+        {tag.sort().map((img) => {
           return <PictureCard img={img} pictureStyle="tag-filtered" />;
         })}
       </Row>
-    </Col>
+    </Col>:null}</>
   );
 };
 
