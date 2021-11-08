fbece28dea7d72f1e8135e4a4a2feb321d69889f
Jacob Leon Lyerla <umasour@yahoo.com>
2021-11-08 15:35:07 -0700

    user submissions edited so if we call it form our post view we can render different data and if we call it from our tags view we render the same stuff we were already rendering

--- src/components/userSubmissionTable/userSubmissionTable.component.jsx
+++ src/components/userSubmissionTable/userSubmissionTable.component.jsx
@@ -1,25 +1,39 @@
-import React from 'react'
+import React,{useContext} from 'react'
 import { withRouter } from 'react-router';
-const UserSubmission = ({history,tag}) => {
+import AppContext from '../../context';
+const UserSubmission = ({history,tag,type}) => {
+  const {clearNotifications,user}=useContext(AppContext)
+let count = 0
+  if(tag.unreadComment || tag.unreadLike){
+   count = tag.unreadComment + tag.unreadLike
+  }
+  const checkIfUser=(id)=>{
+    console.log("check",tag)
+    if(tag.user === user.username&&tag.unreadComment+tag.unreadLike>0){
+      clearNotifications(id)
+    }
+    history.push(`/dashboard/trek/${tag._id}`)
+  }
     return (
       
-        <div
-        onClick={()=>history.push(`/dashboard/trek/${tag._id}`)}
-        className="td-container"
+      <>  {tag ?  <div
+        onClick={()=>checkIfUser(tag._id)}
+        className={`td-container ${count>0 ? "notify":""}`}
         style={{ display: "flex", flexDirection: "row" }}
       >
-        <div className="td">{tag.name}</div>
-        <div className="td">
+        
+      {type==="Post"? <div className="td">{count}</div>:<div className="td">{tag.name}</div>}
+        <div  className="td">
           {tag.locationName ? tag.locationName : "location not given"}
         </div>
         <div className="td tag">
         {tag.tag.map(tag=>(
          <div className="tags">{tag}<br/></div>
         ))} </div>     
-      </div>
+      </div>:""}</>
     )
 }
 
 export default withRouter(UserSubmission)
 
-
+ 
