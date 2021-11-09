f89cb68bbb0d1734f37e1cab70e511fce13c98e9
Jacob Leon Lyerla <umasour@yahoo.com>
2021-11-09 00:02:00 -0700

    removed some logic from our render pictures component and made it stateless

--- src/components/dashboard/renderPictures.js
+++ src/components/dashboard/renderPictures.js
@@ -3,44 +3,22 @@ import React, { Component } from "react";
 import { Row } from "reactstrap";
 import PictureCard from "../pictureCard/pictureCard.component.jsx";
 
-class Pictures extends Component {
-
-
-  
-  // our render pictures funtion is pretty cool
-  renderPictures = () => {
-    // first we set up an empty array for the images
-    let arr = [];
-    // if pictures is defined we can go inside and start doing work
-    if (this.props.pictures !== undefined) {
-      // when the props pictures is longer than one we map over them and push thme into our array
-      if (this.props.pictures.length > 0) {
-        this.props.pictures.forEach((picture) => {
+const Pictures = ({ pictures }) => {
+  const renderPictures = () => {
+    const arr = [];
+    if (pictures !== undefined) {
+      if (pictures.length > 0) {
+        pictures.forEach((picture) => {
           arr.push(picture.name);
         });
       }
     }
-    // next we check and make sure that the img tage is defined than we
-    // chec it's length if the length is greater than 0 we set our array to that
-    if (this.props.imgTag !== undefined) {
-      if (this.props.imgTag.length > 0) {
-        arr = this.props.imgTag;
-      }
-    }
-
-    // when our array is longer than zero we than map out the array
-    if (arr.length > 0) {
-      return arr.sort().map((img) => {
-
-        return <PictureCard img={img} />;
-      });
-    } else {
-    }
+    console.log(arr);
+    return arr.sort().map((img) => {
+      return <PictureCard img={img} />;
+    });
   };
-  render() {
-    return (
-      <Row>{this.renderPictures()}</Row>
-    );
-  }
-}
+
+  return <Row>{renderPictures()}</Row>;
+};
 export default Pictures;
