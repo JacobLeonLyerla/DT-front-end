8bf093cb55721f61815bb83b549e281e984bebdc
Jacob Leon Lyerla <umasour@yahoo.com>
2021-11-09 02:03:15 -0700

    removed script additon to app

--- src/App.js
+++ src/App.js
@@ -14,18 +14,7 @@ import TagViewContainer from "./components/containers/tagViewContainer.component
 import CommentContainer from "./components/containers/commentContainer";
 
 const App = () => {
-  useEffect(() => {
-    const script = document.createElement('script');
 
-    script.src = `https://maps.googleapis.com/maps/api/js?key=${process.env.REACT_APP_GOOGLE_MAPS_API_KEY}&libraries=geometry,drawing,places"`
-    script.async = true;
-  
-    document.body.appendChild(script);
-  
-    return () => {
-      document.body.removeChild(script);
-    }
-  }, [])
   return (
     <div className="App">
       <Route exact path="/" component={Landing} />
